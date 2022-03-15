f949bfe2e53c9a114664469b77962ee178230ffe
Melik <melikpapikyan@gmail.com>
2022-03-15 19:56:48 +0400

    Практика , ч3. Используем функции

--- js/script18.js
+++ js/script18.js
@@ -2,10 +2,10 @@
 
 let numberOfFilms;
 
-function start(){
+function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
 }
@@ -23,17 +23,15 @@ const personalMovieDB = {
 };
 
 
-
-
-function rememberMyFilms(){
+function rememberMyFilms() {
 
     for (let i = 0; i < 2; i++) {
         const a = prompt('Один из последних просмотренных фильмов?', ''),
             b = +prompt('Насколько оцените еГо?', '');
-    
+
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
-    
+
         } else {
             i--;
             console.log('error');
@@ -43,14 +41,7 @@ function rememberMyFilms(){
 
 rememberMyFilms();
 
-
-
-
-
-
-
-
-function detectPersonalLevel(){
+function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
         console.log('Просмотрено довольно мало фильмов');
     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -60,10 +51,28 @@ function detectPersonalLevel(){
     } else {
         console.log('произошла ошибка');
     }
-   
+
 }
 
 detectPersonalLevel();
 
+
+function showMyDB(hidden) {
+    if (hidden != false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.private);
+
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+
+}
+
+writeYourGenres();
 
 console.log(personalMovieDB);
