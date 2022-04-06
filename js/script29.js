757ba0bb02e0145ce3a56e621e604f05c24c2101
Melik <melikpapikyan@gmail.com>
2022-04-06 12:38:43 +0400

    Действия с элементами на странице

--- js/script29.js
+++ js/script29.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart');
+const box = document.getElementById('box'), // poluchaet element iz html s identifikatorom box
+      btns = document.getElementsByTagName('button'), // poluchaet element iz html po tegu
+      circles = document.getElementsByClassName('circle'), // poluchaet elent html po imeni klasa s ogranichenimi prototipami
+      hearts = document.querySelectorAll('.heart'), // poluchaet vse elementi iz html po klasu hearts
+      oneHeart = document.querySelector('.heart'), // poluchaem pervi elementi iz html 
+      wrapper = document.querySelector('.wrapper');// poluchaem pervi elementi iz html
 
+     // console.log(hearts);
+      /*
       box.style.backgroundColor = 'blue';
       box.style.width = '500px';
 
@@ -19,5 +22,21 @@ const box = document.getElementById('box'),
     }*/
 
       hearts.forEach(item =>{
-        item.style.background = 'blue';
+        item.style.background = 'blue'; // perebiraet vse serdechki v cikle forEach i stilizuet kajdi element v cvet blue
       });
+
+
+      const div = document.createElement('div'); // sozdaet v js element div
+      div.classList.add('black'); // dobavlyaet class black k sozdanomu elementu div
+
+     // document.querySelector('.wrapper').append(div);
+     
+      //wrapper.append(div);
+      //wrapper.prepend(div);
+
+      //console.log(hearts);
+
+     // hearts[0].before(div);
+     hearts[0].after(div); // dobavlyaet k ele
+
+
