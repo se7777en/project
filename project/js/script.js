7180f6395f9382d3e62e748c7b09bf958a490533
Melik <melikpapikyan@gmail.com>
2022-04-21 22:18:31 +0400

    Упражнение по написанию кода

--- project/js/script.js
+++ project/js/script.js
@@ -23,3 +23,101 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+        promo = document.querySelector('.promo__bg'),
+        genre = promo.querySelector('.promo__genre'),
+        moviesList = document.querySelector('form.add'),
+        addinput = moviesList.querySelector('.adding__input'),
+        checkbox = moviesList.querySelector('[type="checkbox"]'),
+        films = document.querySelector('.promo__interactive-list');
+       
+
+
+
+
+
+        
+    function favorite(checkbox){
+        if(checkbox.checked){
+            console.log('added to favorite!');
+        }
+    }
+
+    function sortArr(arr){
+        arr.sort();
+    }
+    
+    moviesList.addEventListener('submit', e =>{
+        e.preventDefault();
+        favorite(checkbox);      
+        
+        let inp = addinput.value;
+        if(inp){
+        if(inp.length > 21){
+            inp = `${inp.substring(0,22)}...`;
+        }
+
+        movieDB.movies.push(inp);
+
+        createMovielist(films,movieDB.movies);
+    }
+
+    });
+
+
+    function createMovielist(film,parrent){
+        film.innerHTML = "";
+        sortArr(parrent);
+
+        parrent.forEach((item,i)=>{
+            film.innerHTML += `<li class="promo__interactive-item">${i + 1} ${item}
+            <div class="delete"></div>
+        </li>`;
+
+        });
+
+        document.querySelectorAll('.delete').forEach((item,i)=>{
+
+            item.addEventListener('click',()=>{
+                item.parentElement.remove();
+                parrent.splice(i,1);
+                
+                createMovielist(film,parrent);
+            });
+
+        });
+        
+    }
+
+
+
+
+
+
+    const makechanges = () => {
+        genre.textContent = "драма";
+        promo.style.backgroundImage = 'url("img/bg.jpg")';      
+    };
+
+   makechanges();
+
+
+
+    function removeAdds(adv) {
+        adv.forEach((item) => {
+            item.remove();
+        });
+    }
+
+    removeAdds(adv);
+    createMovielist(films,movieDB.movies);
+
+
+
+
+
+
+});
