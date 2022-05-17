0b95b6582381d1a982c24a84ebfbf38df2e58be6
Melik <melikpapikyan@gmail.com>
2022-05-17 18:08:54 +0400

    Создаем таймер обратного отсчета на сайте

--- JavaScript в работе/Создаем табы в новом проекте/js/script.js
+++ JavaScript в работе/Создаем табы в новом проекте/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
+    let start = new Date();
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabParent = document.querySelector('.tabheader__items'),
         tabContent = document.querySelectorAll('.tabcontent');
@@ -8,9 +10,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function HideTabsContent() {
         tabContent.forEach((item) => {
-           // item.style.display = 'none';
-           item.classList.add('hide');
-           item.classList.remove('show','fade');
+            // item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach((item) => {
@@ -21,9 +23,9 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function ShowTabsContent(i = 0) {
-       // tabContent[i].style.display = 'block';
-       tabContent[i].classList.add('show','fade');
-       tabContent[i].classList.remove('hide');
+        // tabContent[i].style.display = 'block';
+        tabContent[i].classList.add('show', 'fade');
+        tabContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
 
     }
@@ -44,7 +46,65 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
+    // Timer
 
 
+
+    const deadline = '2022-06-11';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+ let end = new Date();
+console.log(end - start);  
 
 });
+
