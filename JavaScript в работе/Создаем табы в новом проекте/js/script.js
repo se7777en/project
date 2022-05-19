56899972e60643c8d447b8b635ce3a1134f405e1
Melik <melikpapikyan@gmail.com>
2022-05-20 01:50:26 +0400

    Создаем модальное окно

--- JavaScript в работе/Создаем табы в новом проекте/js/script.js
+++ JavaScript в работе/Создаем табы в новом проекте/js/script.js
@@ -107,13 +107,17 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', function() {
+
+        function openModal(){
             modal.classList.add('show');
             modal.classList.remove('hide');
             // Либо вариант с toggle - но тогда назначить класс в верстке
             document.body.style.overflow = 'hidden';
-        });
+            clearInterval(settimeModal);
+        }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -137,7 +141,16 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
         }
     });
 
+    const settimeModal = setInterval(openModal,5000);
 
+function showModalByScroll(){
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+}
+
+window.addEventListener('scroll', showModalByScroll);
 
 
 let end = new Date();
