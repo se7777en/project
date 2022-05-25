8b6f013d09020de5845dc23ca661b44dfcdba511
Melik <melikpapikyan@gmail.com>
2022-05-25 11:18:11 +0400

    AJAX и общение с сервером

---
+++ Продвинутый JavaScript/AJAX и общение с сервером/js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+
+
+
+
+//sobitie change voznikaet togda kogda input vixodit iz fokusa
+inputRub.addEventListener('input', () => {
+    const reques = new XMLHttpRequest();
+
+    //parametri
+    //1) method
+    //2) url put k failu , put formiruetsya otnositelno index.html
+    //3) async - robota v asinxronnom rejime
+    //4)login
+    //5) pass 
+
+    reques.open('GET', 'js/current.json'); // open - sobiraet nastroiki kotorie v budushem budet delat zapros
+    reques.setRequestHeader('Content-type', 'application/json; charset=utf-8'); //sho imenno mi otpravlyaem JSON,kartinki i td.. dlya etogo est http zagolovki
+    reques.send();// kogda mi otpravlyaem GET zapros mi prosto zaprashivaem informaciu
+    
+
+    reques.addEventListener('load', ()=> {
+// readystatechange - otslejivaet status gotovnosti nashego obiekta ono budet vizivatsya neskolko raz
+//est esho load
+// load - poproshe on srabativaet odin raz kogda zapros uje gotov zagruzilsya
+if(reques.readyState === 4 && reques.status === 200) {
+    console.log(reques.response);
+    const data = JSON.parse(reques.response);
+    inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+}  else{
+    inputUsd.value = "Chto-to poshlo ne tak";
+} 
+
+});
+
+
+    // v POST zaprose otpravlyaem body telo 
+    
+
+    //status 404 200 i td..
+    //statusText - tekstovoe opisanie otveta ot servera OK,Created,Accepted i td..
+    //response - otvet
+    //readyState - tekushee sostoyanie zaprosa ot 0 do 4 ,4 - operaciya polnostiu zavershena
+    
+});
