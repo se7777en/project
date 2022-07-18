fe69dab114ce519b75d161590f03c93e5b2210cd
Melik <melikpapikyan@gmail.com>
2022-07-18 12:36:42 +0400

    Prepos_less1

---
+++ css/prepos/#source/js/js3.js
@@ -0,0 +1,4 @@
+function hellow3(a){
+    return a;
+    }
+    console.log(hellow3(44));
---
+++ css/prepos/js/main.js
@@ -0,0 +1,14 @@
+
+/*obiedinyaet vse faili v odin*/
+function hellow1(a){
+return a;
+}
+console.log(hellow1(25));  
+function hellow2(a){
+    return a;
+    }
+    console.log(hellow2(30));
+function hellow3(a){
+    return a;
+    }
+    console.log(hellow3(44));
