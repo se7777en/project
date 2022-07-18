be2f71e35b08e3365defd5031d51df981b45a478
Melik <melikpapikyan@gmail.com>
2022-07-18 12:36:42 +0400

    Prepos_less1

---
+++ css/prepos/#source/js/js2.js
@@ -0,0 +1,4 @@
+function hellow2(a){
+    return a;
+    }
+    console.log(hellow2(30));
---
+++ css/prepos/#source/js/main.js
@@ -0,0 +1,5 @@
+//@prepros-append js.js
+//@prepros-append js2.js
+//@prepros-append js3.js
+
+/*obiedinyaet vse faili v odin*/
