e427ef1ff1b956c43b0c308ee3d7d2b96ff3618a
Melik <melikpapikyan@gmail.com>
2022-07-25 11:36:38 +0400

    Prepos_less1

---
+++ css/prepos/#source/js/js-dist.js
@@ -0,0 +1,4 @@
+function hellow1(a){
+return a;
+}
+console.log(hellow1(25));  
--- Продвинутый JavaScript/AJAX и общение с сервером/js/script.js
+++ Продвинутый JavaScript/AJAX и общение с сервером/js/script.js
@@ -9,7 +9,7 @@ const inputRub = document.querySelector('#rub'),
 //sobitie change voznikaet togda kogda input vixodit iz fokusa
 inputRub.addEventListener('input', () => {
     const reques = new XMLHttpRequest();
-
+    reques.open('GET', 'js/current.json'); // open - sobiraet nastroiki kotorie v budushem budet delat zapros
     //parametri
     //1) method
     //2) url put k failu , put formiruetsya otnositelno index.html
@@ -17,12 +17,12 @@ inputRub.addEventListener('input', () => {
     //4)login
     //5) pass 
 
-    reques.open('GET', 'js/current.json'); // open - sobiraet nastroiki kotorie v budushem budet delat zapros
+    
     reques.setRequestHeader('Content-type', 'application/json; charset=utf-8'); //sho imenno mi otpravlyaem JSON,kartinki i td.. dlya etogo est http zagolovki
     reques.send();// kogda mi otpravlyaem GET zapros mi prosto zaprashivaem informaciu
     
 
-    reques.addEventListener('load', ()=> {
+    reques.addEventListener('load', () => {
 // readystatechange - otslejivaet status gotovnosti nashego obiekta ono budet vizivatsya neskolko raz
 //est esho load
 // load - poproshe on srabativaet odin raz kogda zapros uje gotov zagruzilsya
@@ -46,3 +46,15 @@ if(reques.readyState === 4 && reques.status === 200) {
     //readyState - tekushee sostoyanie zaprosa ot 0 do 4 ,4 - operaciya polnostiu zavershena
     
 });
+
+//spisok kodov sostoyaniya 
+//https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%BA%D0%BE%D0%B4%D0%BE%D0%B2_%D1%81%D0%BE%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D1%8F_HTTP
+
+
+//XMLHttpRequest.readyState
+//https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest/readyState
+
+
+
+//addEventListener('input', - sobitie proisxodit pri vvode dannix  
+//addEventListener('change', - sobitie proisxodit kagda pole input ne v fokuse
