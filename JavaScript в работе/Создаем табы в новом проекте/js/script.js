9383ccf0621736298f52c564aa381fff3a232bfe
Melik <melikpapikyan@gmail.com>
2022-10-21 19:21:16 +0400

    modifed for slider

--- JavaScript в работе/Создаем табы в новом проекте/js/script.js
+++ JavaScript в работе/Создаем табы в новом проекте/js/script.js
@@ -806,23 +806,6 @@ console.log(`Vremya skripta zanyalo ${end - start} millisekund`);*/
 
 
 /////////////////////////////////////////// calc
-    const result = document.querySelector('.calculating__result span');
-    let sex, height, weight, age, ratio;
-
-    function calcResult() {
-        if (!sex || !height || !weight || !age || !ratio) {
-
-            result.textContent = '____';
-            return;
-        }
-
-        if (age === "female") {
-            result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
-        } else {
-            result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
-        }
-
-    }
-    calcResult();
+   
 
 });
