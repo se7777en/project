cdecb530ebfd6726bb9ca7cf2634db5d67b7fbf0
Melik <melikpapikyan@gmail.com>
2022-10-31 11:13:23 +0400

    Calc_modifed2

--- JavaScript в работе/Создаем табы в новом проекте/js/script.js
+++ JavaScript в работе/Создаем табы в новом проекте/js/script.js
@@ -812,6 +812,49 @@ const result = document.querySelector('.calculating__result span');
 let sex = 'female',
 height, weight, age, 
 ratio = '1.375';
+//let sex, height, weight, age, ratio;
+
+
+if(localStorage.getItem('sex')){
+    sex = localStorage.getItem('sex');
+}else{
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+}
+
+if(localStorage.getItem('ratio')){
+    ratio = localStorage.getItem('ratio');
+}else{
+    ratio = 1.375;
+    localStorage.setItem('ratio', '1.375');
+}
+
+
+function initLocalStorage(selector, activeClass) {
+    const storage = document.querySelectorAll(selector);
+
+    storage.forEach((item) => {
+        item.classList.remove(activeClass);
+
+        if (item.getAttribute('id') === localStorage.getItem('sex')) {
+            item.classList.add(activeClass);
+           // sex = localStorage.getItem('sex'); 
+        }
+
+        if (item.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+            item.classList.add(activeClass);
+           // ratio = localStorage.getItem('ratio');
+        }
+
+    });
+}
+
+initLocalStorage('#gender div', 'calculating__choose-item_active');
+initLocalStorage('.calculating__choose_big div', 'calculating__choose-item_active');
+
+
+
+
 
 function GetResult() {
     if (!sex || !height || !weight || !age || !ratio) {
@@ -831,19 +874,21 @@ GetResult();
 
 function calcData(parent, activClass) {
 
-    const result = document.querySelectorAll(`${parent} div`);
+    const result = document.querySelectorAll(parent);
     result.forEach((item) => {
         item.addEventListener('click', (e) => {
             if (e.target.getAttribute('data-ratio')) {
                 ratio = +e.target.getAttribute('data-ratio');
+                localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
             } else {
                 sex = e.target.getAttribute('id');
+                localStorage.setItem('sex', e.target.getAttribute('id'));
             }
 
             result.forEach((item) => {
                 item.classList.remove(activClass);
             });
-            item.classList.add(activClass);
+            e.target.classList.add(activClass);
 
             GetResult();
         });
@@ -852,8 +897,8 @@ function calcData(parent, activClass) {
 
 }
 
-calcData('.calculating__choose_big', 'calculating__choose-item_active');
-calcData('#gender', 'calculating__choose-item_active');
+calcData('.calculating__choose_big div', 'calculating__choose-item_active');
+calcData('#gender div', 'calculating__choose-item_active');
 
 
 
