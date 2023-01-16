5669643c6918843a86e4d1b2160064584e6e0f81
Melik <melikpapikyan@gmail.com>
2023-01-16 12:28:05 +0400

    figma_template_AVTR

--- css/AVTR/js/main.js
+++ css/AVTR/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img src="images/arrow-left.svg" alt="" class="arrow arrow-left">',
+        nextArrow: '<img src="images/arrow-right.svg" alt="" class="arrow arrow-right">',
 
+    });
 });
