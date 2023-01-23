bfe89394152a0b913887be9587dc8c2d8713d117
Melik <melikpapikyan@gmail.com>
2023-01-23 12:40:40 +0400

    figma_template_AVTR

--- css/AVTR/js/main.js
+++ css/AVTR/js/main.js
@@ -2,7 +2,7 @@ $(function(){
     $('.design-slider').slick({
         dots: false,
         slidesToShow: 4,
-        variableWidth: true,
+        variableWidth: false,
         prevArrow: '<img src="images/arrow-left.svg" alt="" class="arrow arrow-left">',
         nextArrow: '<img src="images/arrow-right.svg" alt="" class="arrow arrow-right">',
 
