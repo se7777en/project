4b66d7c02ef48799d3d020e5b532523cccdea42e
Melik <melikpapikyan@gmail.com>
2023-04-17 18:18:01 +0400

    Karshering_template

--- css/Каршеринг/js/main.js
+++ css/Каршеринг/js/main.js
@@ -12,4 +12,16 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 1
     });
+
+
+
+$('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+});
+
+// document.querySelector('.menu__btn').addEventListener('click',()=>{
+//     document.querySelector('.menu__list').classList.toggle('menu__list--active');
+// });
+
+
 });
