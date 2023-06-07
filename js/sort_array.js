eac9728a7f1cd0ccf1b7ff71d8104ed6f5140f60
Melik <melikpapikyan@gmail.com>
2023-06-07 23:07:11 +0400

    -aElysium__template

---
+++ js/sort_array.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* sort array*/
+const array = [1, 7, 9, -1, 15, 88];
+
+function quickSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    let pivotIndex = Math.floor(array.length / 2);
+    let pivot = array[pivotIndex];
+    let less = [];
+    let greater = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i === pivotIndex)
+            continue;
+        if (array[i] < pivot) {
+            less.push(array[i]);
+        } else {
+            greater.push(array[i]);
+        }
+    }
+
+    return [...quickSort(less), pivot, ...quickSort(greater)];
+}
+
+console.log(quickSort(array));
+
