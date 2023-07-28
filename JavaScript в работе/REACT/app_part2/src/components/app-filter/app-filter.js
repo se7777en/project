0b7ad6f203f9b97d139921814e3d93401ee701f3
Melik <melikpapikyan@gmail.com>
2023-07-28 16:20:29 +0400

    -ascss

--- JavaScript в работе/REACT/app_part2/src/components/app-filter/app-filter.js
+++ JavaScript в работе/REACT/app_part2/src/components/app-filter/app-filter.js
@@ -6,14 +6,19 @@ const AppFilter = (props) => {
     const btnData = [
         {name:'all', label: 'Все сотрудники'},
         {name:'rise', label: 'На повышение'},
-        {name:'moreThen1000', label: 'З/П больше 1000$'}
+        {name:'moreThen1000', label: 'З/П больше 1000$', colored: true}
     ];
 
-    const buttons = btnData.map(({name, label}) => {
+    const buttons = btnData.map(({name, label, colored}) => {
         const result = props.filter === name
         const clazz = result ? 'btn-light' : 'btn-outline-light' 
+        const style = colored ? {color: 'red'}: null;
         return (
-        <button className={`btn ${clazz}`} type="button" key={name} onClick={() => props.upName(name)}>
+        <button className={`btn ${clazz}`} 
+                type="button" 
+                key={name} 
+                onClick={() => props.upName(name)}
+                style={style}>
             {label}
         </button>
         )
