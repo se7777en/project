65fcd55b475e55fd2daaf20e0cde81fb098ef7b9
Melik <melikpapikyan@gmail.com>
2023-08-28 21:11:17 +0400

    -aSlider

--- css/Тестовое задание2/js/script.js
+++ css/Тестовое задание2/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(){
+$(document).ready(function () {
+    $('.header__burger').click(function () {
         $('.header__burger, .menu').toggleClass('active');
         $('body').toggleClass('lock');
 
@@ -10,16 +10,70 @@ $(document).ready(function(){
 
 
 /*Slick__Slider*/
-$(document).ready(function(){
-	$('.service__wrapper').slick({
-        arrows:true,
-		dots:false,
-		slidesToShow:1,
-		autoplay:false,
-		speed:1000,
-		autoplaySpeed:800,
-        
+$(document).ready(function () {
+    $('.service__wrapper').slick({
+        arrows: true, /*strelki*/
+        dots: true, /*tochki dlya slaida*/
+        slidesToShow: 1,/*kolichestvo slaidov kotoroe otobrajaetsya*/
+        /*------*/
+        autoplay: false,/* avtoperekluchenie slaidov pri infinite: true prolistivaetsya krasivee*/
+        autoplaySpeed: 800, /*vremya autopereklucheniya slaidov po umolchaniu 3000 3s*/
+        speed: 1000,/*skorost prokruchivaniya po umolchaniu 300ms*/
+        /*-------*/
+        adaptiveHeight: true, /* dlya adaptacii po visote*/
+        slidesToScroll: 1, /*kolichestvo slaidov prokruchivaemoe za odin raz*/
+        easing: 'ease', /*animaciya perexoda slaida po umolchaniu linear  https://fls.guru/csstransition.html*/
+        infinite: true, /*buder li slaider beskonechni po umolchaniu true*/
+        initialSlide: 0, /* startovi slaid po umolchaniu 0*/
+
+        pauseOnFocus: false, /*ostanavlivaet prokrutku pri fokuse*/
+        pauseOnHover: false, /*ostanavlivaet prokrutku pri hovere*/
+        pauseOnDotsHover: false, /*ostanavlivaet prokrutku pri hovere na dotsi*/
+
+        draggable: false, /* swaipat listat na PK mishkoi strelkomi po umolchaniu vklucheno*/
+        swipe: false, /* swaipat listat na mobilnom ustroistve po umolchaniu vklucheno*/
+        touchThreshold: 10, /* kak proisxodit swaip pri kasanii*/
+        touchMove: true, /* peredvigat palcem ne smojem*/
+        waitForAnimate: true, /*jdet okonchaniya animacii pri perekluchenii slaida*/
+
+        centerMode: false, /* po umolchaniu false osnovnoi slaider po centru i emu dobavlyaetsya klass slick-center*/
+        variableWidth: false, /* rastoyanie mejdu slaidami ubiraetsya robotaet s centerMode luchshe*/
+        rows: 1, /*kolichestvo ryadov po umolchaniu 1*/
+        slidesPerRows: 1,/*kolichestvo slaidov v ryadu*/
+
+
+        /*========*/
+        vertical: false, /* nujno sdelat element blochnim a u nas flex .service__wrapper .slick-track i ukazivaem fiksirovanuu visotu bloku*/
+        verticalSwiping: false, /*vertikalni swaip*/
+        /*========*/
+
+        // fade: true, /*slaidi budut zamenyatsya a ne prolistivatsya*/
+
+        //asNavFor: ".slider", /*kogda 2 slaidera, mojno ix svyazat u 1 slaida ukazat klass 2 slaida u 2 slaida ukazat klass 1 slaida i pri skrole 2 slaida skrolitsya budut oba slaida*/
+
+
+        mobileFirst: true, /* vkluchaet mobile first dlya responsive:*/
+
+        //  appendArrows: $('.content'), /*peremeshaet strelki v opredeleni block*/
+        //  appendDots: $('.content') /*peremeshaet dotsi v opredeleni block*/
+    });
+
+    // $('.service__wrapper').slick('setPosition'); /* vstrexnet slaider i postavit na mesto*/
+
+    /*metodi*/
+    $('.service__wrapper_').slick('goTo', 0); /*prolistnet do 3 slaida*/
+
+    $('.service__wrapper_').slick('slickPrev');/*prolistnet slaid nazad*/
+    $('.service__wrapper_').slick('slickNext');/*prolistnet slaid vpered*/
+
+    $('.service__wrapper_').slick('slickPlay');/*zapustit slaider*/
+
+
+    $('.service__wrapper_').slick('slickNext');/*prolistnet slaid vpered*/
+
+
+    $('.link_l').click(function (event) {
+        $('.service__wrapper').slick('slickPause');/*postavit na pauzu*/
     });
 });
 
-  
