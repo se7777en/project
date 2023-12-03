7490cb8692d3eec9d0244e098ab704aa6c5fe190
Melik <melikpapikyan@gmail.com>
2023-12-03 22:00:06 +0400

    -aEdenTemplate

---
+++ css/EdenTravel_template/src/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.popular-destinations__items').slick({
+    arrows: true, /*strelki*/
+    slidesToShow: 4,
+    infinite: false,
+    dots: true
+
+  });
+
+  $('.stories-items').slick({
+    arrows: true, /*strelki*/
+    slidesToShow: 2,
+    infinite: false,
+    dots: true
+
+  });
+
+
+});
+
