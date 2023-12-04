6334e930b2201a28bf4921da32cfce14517522c5
Melik <melikpapikyan@gmail.com>
2023-12-04 12:37:51 +0400

    -aEdenTravel_template

--- css/EdenTravel_template/src/js/script.js
+++ css/EdenTravel_template/src/js/script.js
@@ -18,3 +18,15 @@ $(document).ready(function(){
 
 });
 
+
+
+// for burger
+const active = document.querySelector('.burger__menu'),
+      headerMenu = document.querySelector('.header__menu'),
+      body = document.querySelector('body');
+
+      active.addEventListener('click', () => {
+        active.classList.toggle('active');
+        headerMenu.classList.toggle('active');
+        body.classList.toggle('lock');
+      });
