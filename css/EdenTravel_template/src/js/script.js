f970af2f6fd2826403ce3f17b20c36a7afa55183
Melik <melikpapikyan@gmail.com>
2023-12-11 23:21:35 +0400

    -atemplate__EdenTravel

--- css/EdenTravel_template/src/js/script.js
+++ css/EdenTravel_template/src/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $('.destionations__item-inner').slick({
-    arrows: true,
-    /*strelki*/
-    slidesToShow: 4,
-    infinite: false,
-    dots: true,
-    responsive:[
+	$('.destionations__item-inner').slick({
+		arrows: true,
+		/*strelki*/
+		slidesToShow: 4,
+		infinite: false,
+		dots: true,
+		responsive: [
 			{
 				breakpoint: 1376,
 				settings: {
@@ -17,47 +17,55 @@ $(document).ready(function () {
 				breakpoint: 1090,
 				settings: {
 					arrows: false,
-          slidesToShow: 3
+					slidesToShow: 3
 				}
 			},
 
-      {
+			{
 				breakpoint: 851,
 				settings: {
 					arrows: false,
-          slidesToShow: 2
+					slidesToShow: 2
 				}
 			},
 
-      {
+			{
 				breakpoint: 572,
 				settings: {
 					arrows: false,
-          slidesToShow: 1
+					slidesToShow: 1
 				}
 			}
 		]
 
-  });
+	});
 });
 
 $(document).ready(function () {
-$('.stories-items').slick({
-  arrows: true, /*strelki*/
-  slidesToShow: 2,
-  infinite: false,
-  dots: true
+	$('.stories-items').slick({
+		arrows: true, /*strelki*/
+		slidesToShow: 2,
+		infinite: false,
+		dots: true,
+		responsive: [
+			{
+				breakpoint: 586,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
 
-});
+	});
 });
 
 // for burger
 const active = document.querySelector('.burger__menu'),
-  headerMenu = document.querySelector('.header__menu'),
-  body = document.querySelector('body');
+	headerMenu = document.querySelector('.header__menu'),
+	body = document.querySelector('body');
 
 active.addEventListener('click', () => {
-  active.classList.toggle('active');
-  headerMenu.classList.toggle('active');
-  body.classList.toggle('lock');
+	active.classList.toggle('active');
+	headerMenu.classList.toggle('active');
+	body.classList.toggle('lock');
 });
