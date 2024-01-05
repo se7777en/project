03bbfcda81ec4cce5691a985d063c8aa0fddcb21
Melik <melikpapikyan@gmail.com>
2024-01-06 02:52:31 +0400

    -areact_proekt

--- JavaScript в работе/REACT/employees_template_zagatovka/src/components/employees-list/employees-list.js
+++ JavaScript в работе/REACT/employees_template_zagatovka/src/components/employees-list/employees-list.js
@@ -2,12 +2,30 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({ data, onDelete, onToggleProp, onSallaryChange}) => {
+
+
+    let employeer = data.map((item) => {
+        
+        const { id, ...proplist } = item;
+        return (
+            <EmployeesListItem onDelete={() => onDelete(id)} key={id} {...proplist}
+                // onToggleIncrease={()=>onToggleIncrease(id)}
+                // onToggleRice={()=>onToggleRice(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+                onSallaryChange={(e)=>onSallaryChange(id, e.currentTarget.value)}
+            />
+            //name = {item.name} sallary = {item.sallary} key = {item.key} increase = {item.increase}
+        );
+    });
+
+    if(employeer.length === 0) {
+        employeer = <h3>Data not found</h3>;
+    }
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {employeer}
         </ul>
     )
 }
--- JavaScript в работе/REACT/employees_template_zagatovka/src/components/search-panel/search-panel.js
+++ JavaScript в работе/REACT/employees_template_zagatovka/src/components/search-panel/search-panel.js
@@ -1,11 +1,30 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input type="text"
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    updateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({term})
+        this.props.updateSearch(term) // peredaem e.target.value naverx
+    }
+
+    render() {
+        return (
+            <input type="text"
                 className="form-control search-input"
-                placeholder="Найти сотрудника"/>
-    )
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.updateSearch}
+            />
+        )
+    }
 }
 
 export default SearchPanel;
