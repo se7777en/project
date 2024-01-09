e400430ff19f764d8af6bd3509138de98aebaeea
Melik <melikpapikyan@gmail.com>
2024-01-09 11:33:12 +0400

    -atests

---
+++ css/guizz app/src/js/script.js
@@ -0,0 +1,207 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    const testData = [
+        {
+            question: 'Какой метод для фильтрации массива в JavaScript?',
+            a: 'filter',
+            b: 'sift',
+            c: 'sort',
+            d: 'select',
+            rightAnswer: 'a'
+        },
+        {
+            question: 'Как называется метод для добавления элемента в начало массива в JavaScript?',
+            a: 'prepend',
+            b: 'push',
+            c: 'unshift',
+            d: 'insert',
+            rightAnswer: 'c'
+        },
+        {
+            question: 'Какое ключевое слово используется для объявления константы в JavaScript?',
+            a: 'constant',
+            b: 'let',
+            c: 'var',
+            d: 'const',
+            rightAnswer: 'd'
+        },
+        {
+            question: 'Какой метод используется для объединения двух массивов в JavaScript?',
+            a: 'merge',
+            b: 'concatenate',
+            c: 'join',
+            d: 'combine',
+            rightAnswer: 'b'
+        },
+        {
+            question: 'Как называется функция, передаваемая в метод массива для выполнения операции над каждым элементом?',
+            a: 'map',
+            b: 'iterate',
+            c: 'loop',
+            d: 'execute',
+            rightAnswer: 'a'
+        },
+        {
+            question: 'Какой оператор используется для проверки неравенства по значению и типу в JavaScript?',
+            a: '!=',
+            b: '!==',
+            c: '==',
+            d: '/=',
+            rightAnswer: 'b'
+        },
+        {
+            question: 'Какой метод используется для удаления последнего элемента из массива в JavaScript?',
+            a: 'pop',
+            b: 'remove',
+            c: 'delete',
+            d: 'exclude',
+            rightAnswer: 'a'
+        },
+        {
+            question: 'Как называется цикл, который выполняется хотя бы один раз в JavaScript?',
+            a: 'for',
+            b: 'while',
+            c: 'do-while',
+            d: 'loop',
+            rightAnswer: 'c'
+        },
+        {
+            question: 'Какой тип данных используется для представления десятичных чисел в JavaScript?',
+            a: 'decimal',
+            b: 'float',
+            c: 'int',
+            d: 'number',
+            rightAnswer: 'b'
+        },
+        {
+            question: 'Какой метод используется для преобразования строки в массив в JavaScript?',
+            a: 'split',
+            b: 'slice',
+            c: 'separate',
+            d: 'divide',
+            rightAnswer: 'a'
+        }
+
+    ];
+    //testData
+    let counter = 0;
+    const inputs = document.querySelectorAll('.list input'),
+        labels = document.querySelectorAll('.list label'),
+        ques = document.querySelector('.form__header'),
+        submit = document.querySelector('.btn');
+
+
+    const updateQuestion = () => {
+        let check = false;
+        inputs.forEach((item) => {
+            if(item.checked) check = true;
+        })
+         if(check === false && counter > 0) return;
+        if (counter >= testData.length -1) return;
+
+        const data = testData[counter];
+        ques.innerHTML = data.question;
+        labels.forEach((item, i) => {
+            let txt = '';
+            if (i === 0) {
+                txt = data.a;
+            }
+            if (i === 1) {
+                txt = data.b;
+            }
+            if (i === 2) {
+                txt = data.c;
+            }
+            if (i === 3) {
+                txt = data.d;
+            }
+            item.innerText = txt;
+        });
+
+
+       
+        
+        inputs.forEach((item) => {
+            item.checked = false;
+        })
+
+       
+
+    }
+
+    updateQuestion();
+
+
+    submit.addEventListener('click', updateQuestion);
+
+    let right = 0;
+    let wrong = 0;
+    inputs.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            if (e.target.checked) {
+                // label value === rightAnswer
+                const yourAnswer = e.target.id;
+                const rightAnswer = testData[counter].rightAnswer;
+                //console.log(`rightAnswer = ${testData[counter].rightAnswer} \n Selected = ${lab}`);
+
+                if (rightAnswer === yourAnswer) {
+                    right += 1;
+                } else {
+                    wrong += 1;
+                }
+
+                console.log(`right = ${right} wrong = ${wrong} yourAnswer = ${yourAnswer} rightAnswer = ${rightAnswer} counter = ${counter}`);
+
+            }
+        })
+    })
+
+
+
+
+
+
+    // let counter = 0;
+    // const question = document.querySelector('.form__header'),
+    //     answ_a = document.querySelector('.lab1'),
+    //     answ_b = document.querySelector('.lab2'),
+    //     answ_c = document.querySelector('.lab3'),
+    //     answ_d = document.querySelector('.lab4'),
+    //     submit = document.querySelector('.btn');
+
+    // const inputs = document.querySelectorAll('.list input');
+
+
+    // inputs.forEach((item) => {
+    //     item.addEventListener('click', (e) => {
+    //         console.log(e.target.checked);
+    //     })
+    // })
+
+    // const SetData = (id) => {
+    //     question.innerHTML = testData[id].question;
+    //     answ_a.innerHTML = testData[id].a;
+    //     answ_b.innerHTML = testData[id].b;
+    //     answ_c.innerHTML = testData[id].c;
+    //     answ_d.innerHTML = testData[id].d;
+    // };
+
+    // SetData(counter);
+
+
+    // submit.addEventListener('click', () => {
+    //     counter++;
+    //     SetData(counter);
+
+    //     // console.log('e');
+    // });
+
+
+
+
+    // testData.forEach((item) => {
+
+    // });
+
+
+});
