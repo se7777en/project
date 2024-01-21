5032109108de527c98d78bf1903789652b923b5e
Melik <melikpapikyan@gmail.com>
2024-01-21 18:28:57 +0400

    -aMarvell

--- JavaScript в работе/REACT/Marvell/src/components/randomChar/RandomChar.js
+++ JavaScript в работе/REACT/Marvell/src/components/randomChar/RandomChar.js
@@ -1,16 +1,15 @@
 import { Component } from 'react';
+import Spinner from '../spinner/Spinner'
+import ErrorMessage from '../errorMessage/errorMessage';
+import MarvelService from '../../services/MarvelService';
+
 import './randomChar.scss';
 // import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-import MarvelService from '../../services/MarvelService';
+
 
 class RandomChar extends Component {
-
-    constructor (props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
         // name: null,
@@ -19,27 +18,57 @@ class RandomChar extends Component {
         // homepage: null,
         // wiki: null
 
-        char: {}
+        char: {},
+        loading: true,
+        error: false
 
     }
 
     marvelService = new MarvelService();
 
-    onChatLoaded = (char) => {
-        this.setState({char})
+
+    componentDidMount() { // vse setevie zaprosi otsuda
+        this.updateChar();
+        //this.timeId = setInterval(this.updateChar, 3000);
+        //this.updateChar();
+        console.log('Mount');
+    }
+
+    componentWillUnmount() { // eto kogda komponent udalyaetsya so stranici
+        clearInterval(this.timeId);
+        console.log('Unmount');
+    }
+
+    onCharLoaded = (char) => {
+        console.log('update');
+        this.setState({ char, loading: false })
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        
+        this.onCharLoading();
         this.marvelService
-        .getCharacter(id)
-        .then(this.onChatLoaded)
+            .getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError)
 
         // .getAllCharacters()
         // .then((res) => console.log(res));
 
-//////////////////////////////////////////
+        //////////////////////////////////////////
         // this.marvelService.getCharacter(id).then(res => {
         //     // console.log(res.data.results[0].urls[0].url);
         //     this.setState(res
@@ -55,26 +84,18 @@ class RandomChar extends Component {
     }
 
     render() {
-        const { char: {name, description, thumbnail, homepage, wiki} } = this.state;
+        console.log('render');
+        const { char, loading, error } = this.state;
+
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View char={char} /> : null;
+
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img" />
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">wiki</div>
-                            </a>
-                        </div>
-                    </div> 
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br />
@@ -83,7 +104,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -91,6 +112,33 @@ class RandomChar extends Component {
             </div>
         )
     }
+}
+
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki } = char;
+    let style = { 'objectFit': 'cover' }
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        style = { 'objectFit': 'contain' }
+    }
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={style} />
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RandomChar;
--- JavaScript в работе/REACT/Marvell/src/services/MarvelService.js
+++ JavaScript в работе/REACT/Marvell/src/services/MarvelService.js
@@ -1,3 +1,5 @@
+//import { v4 as uuidv4 } from 'uuid';
+
 class MarvelService {   //funkciya polucheniya Api v object
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=4d466deb3be421a9b5ab16900a40f398';
@@ -18,15 +20,18 @@ class MarvelService {   //funkciya polucheniya Api v object
         return res.data.results.map(this._transformCharaster);
     }
 
-    getCharacter = async (id) => { /*personaji*/
+    getCharacter = async (id) => { /*personaj*/
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharaster(res.data.results[0]);
     }
 
     _transformCharaster = (char) => {
+        // let descr = char.description;
+        // if(descr.length < 1) descr = 'Data not Found!'
         return {
+            id: char.id,
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'Data not found!',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
