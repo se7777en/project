abaa6f19a9ad872859c7b079630900a4b747116e
Melik <melikpapikyan@gmail.com>
2024-02-21 19:33:07 +0400

    -aplashka

--- css/TMP/gmail_plashka/src/js/script.js
+++ css/TMP/gmail_plashka/src/js/script.js
@@ -2,22 +2,65 @@
 
 window.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
+    const wrapper__item = document.querySelectorAll('.wrapper__item'); // poluchaem vse plashku
 
 
-    const wrapper__item = document.querySelectorAll('.wrapper__item');
-         
+    const itemBoxShadow = (labelChb, classname) => {
+        labelChb.addEventListener('click', () => {
+            labelChb.classList.add(classname); //castom__chb-shadow
+            setTimeout(() => {
+                labelChb.classList.remove(classname);
+            }, 500);
 
-    wrapper__item.forEach((item)=>{
-        const  trashBtn = item.querySelector('.trash__btn');
-        console.log(trashBtn);
+            //show trash
+        })
+    }
 
+    const showTrashBox = (item) => {
+        const trashbox = item.querySelector('.trash__btn');
+        trashbox.style = 'scale: 1';
+    }
+
+    const itemBg = (currentStar, classname) => {
+        currentStar.addEventListener('click', () => {
+            currentStar.classList.toggle(classname);
+            console.log(currentStar);
+        })
+    }
+
+    wrapper__item.forEach((item) => { // perebiraem plashki s itemami
+        const trashBtn = item.querySelector('.trash__btn'), // poluchaem u plashki roditelya korzini
+            labelChb = item.querySelector('.wrapper__item-chb'),
+            starShadow = item.querySelector('.wrapper__item-star'),
+            decoreShadow = item.querySelector('.wrapper__item-decore'),
+            starBgItem = item.querySelector('.wrapper__item-star'),
+            decoreBgItem = item.querySelector('.wrapper__item-decore');
+
+        itemBoxShadow(labelChb, 'castom__chb-shadow');
+        itemBoxShadow(starShadow, 'star-shadow');
+        itemBoxShadow(decoreShadow, 'decore-shadow');
+
+
+        itemBg(starBgItem, 'star-bg');
+        itemBg(decoreBgItem, 'decore-bg');
+        //wrapper__item-decore decore-bg
+        //trash__btn-inner trash-shadow
+
+
+
+
+
+        ////otobrajenie pri hovere na plashku//////
         item.addEventListener('mouseover', () => {
-            trashBtn.style = 'scale: 1';
+            // trashBtn.style = 'scale: 1'; // dobavlyaem svoistva
         });
         item.addEventListener('mouseout', () => {
-            trashBtn.style = 'scale: 0';
-            trashBtn.style.backgroundColor  = '';
+            // trashBtn.style = 'scale: 0'; // dobavlyaem svoistva
+            //  trashBtn.style.backgroundColor  = '';
         });
+        ////otobrajenie pri hovere na plashku//////
+
+
 
     })
 
