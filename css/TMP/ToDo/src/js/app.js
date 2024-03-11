cdeb3c8e74980e9c03e253a3e8c725640270bfd3
Melik <melikpapikyan@gmail.com>
2024-03-11 22:37:54 +0400

    -aToDo

--- css/TMP/ToDo/src/js/app.js
+++ css/TMP/ToDo/src/js/app.js
@@ -10,110 +10,60 @@ window.addEventListener('DOMContentLoaded', (e) => {
         todoInput = document.querySelector('.wrapper .task__input');
 
 
+    // const toDoObj = [
+    //     {
+    //         id: uniqueId(),
+    //         text: 'Temporary todo',
+    //         read: true
+    //     }
+    // ];
+
+
+    // const toDoObj = [];
+    // if (window.localStorage.getItem(toDoObj)) {
+    //     window.localStorage.setItem('toDoObj', toDoObj);
+    // }
+
+
     const uniqueId = () => {
         return 'id-' + Math.random().toString(36).substr(2, 9);
     }
 
-    const toDoObj = [
-        {
-            id: uniqueId(),
-            text: 'Temporary todo',
-            read: true
-        }
-    ];
 
-
-
-
-
-    let objfromStorage = JSON.parse(window.localStorage.getItem('toDoObj'));
-
-    if (!objfromStorage) {
-        window.localStorage.setItem('toDoObj', JSON.stringify(toDoObj));
-        objfromStorage = toDoObj;
-    }
-
-    // let objfromStorage = '';
-    // if (window.localStorage.getItem('toDoObj')) {
-    //     console.log('ok');
-    //     objfromStorage = JSON.parse(window.localStorage.getItem('toDoObj'));
-    // } else {
-    //     console.log('ok2');
-    //     window.localStorage.setItem('toDoObj', JSON.stringify(toDoObj));
-    //     objfromStorage = JSON.parse(window.localStorage.getItem('toDoObj'));
-    // }
-
-
-
-
-    const geDataFromStorage = (tmpObject) => {
+    const geDataFromStorage = () => {
         if (JSON.parse(window.localStorage.getItem('toDoObj'))) {
             return JSON.parse(window.localStorage.getItem('toDoObj'))
-        } else {
-            return tmpObject;
         }
     }
-
-    //geDataFromStorage(toDoObj);
 
     const setDataToStorage = (obj) => {
         window.localStorage.setItem('toDoObj', JSON.stringify(obj));
     }
 
 
-    const onCheckboxClick = (todoChb, objfromStorage1, todoText) => {
-        if (todoChb.checked) {
-            ///////////////////////////////////////////////
-            let taskItem = todoChb.closest('.task__item');
-            let dataId = taskItem.dataset.id;
-            let loop = false;
-            objfromStorage1.forEach((item) => {
-                if (loop) return;
-                if (item.id === dataId) {
-                    item.read = true;
-                    loop = true;
-                }
-            })
-            setDataToStorage(objfromStorage1);
-            if (!todoText.classList.contains('textdecore')) {
-                todoText.classList.add('textdecore');
-            }
-        } else
-        ///////////////////////////////////////////////
-        {
-            let taskItem = todoChb.closest('.task__item');
-            let dataId = taskItem.dataset.id;
-            let loop = false;
-            objfromStorage1.forEach((item) => {
-                if (loop) return;
-                if (item.id === dataId) {
-                    item.read = false;
-                    loop = true;
-                }
-            })
-            setDataToStorage(objfromStorage1);
-
-            if (todoText.classList.contains('textdecore')) {
-                todoText.classList.remove('textdecore');
-            }
-        }
-    }
+  
 
 
 
 
+    let objfromStorage = geDataFromStorage();
+// console.log(objfromStorage);
+
+    /////1)//////
     const wrightItemsToPage = (obj) => {
         // console.log(obj.length);
         let elements = '';
-        if (obj.length > 0) {
+        if (obj && Object.keys(obj).length !== 0) {
+           
             obj.forEach((item) => {
+                console.log(obj);
                 let readStatus = '';
                 if (item.read) { readStatus = 'checked'; } else { readStatus = ''; }
 
-                elements += `<div class="task__item item" data-id="${item.id}">
+                elements += `<div class="task__item item" name="todo" data-id="${item.id}">
     <div class="task__chb">
         <label>
-            <input type="checkbox" class="realchb" ${readStatus}>
+            <input type="checkbox" name="chb" class="realchb" ${readStatus}>
             <span class="castomchb"></span>
         </label>
     </div>
@@ -130,9 +80,6 @@ window.addEventListener('DOMContentLoaded', (e) => {
                 <span class="descr__text-two">Crie tarefas e organize seus itens a fazer</span></div>
         </div>`;
         }
-
-
-
         tasks.innerHTML = '';
         tasks.innerHTML = elements;
     }
@@ -141,119 +88,199 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
 
 
-
-
-
-    /////////////
-    const calcItems = () => {
-
-        let obj = geDataFromStorage(toDoObj);
-        tasksCount.textContent = obj.length;
-
-        let count = 0;
-        obj.forEach((item) => {
-            if (item.read) count += 1;
-        })
-        complatedNum.textContent = `${count} of ${obj.length}`;
-    }
-
-    calcItems();
-    /////////////
-
-
-    const addHoverOnTrash = () => {
-        const trashDecore = document.querySelectorAll('.task__item .task__trash');
-        trashDecore.forEach((item) => {
-            item.addEventListener('mouseenter', () => {
-                item.classList.add('trashdecore');
-                setTimeout(() => {
-                    item.classList.remove('trashdecore');
-                }, 800);
-            });
-        });
-    }
-    addHoverOnTrash();
-
-
-
-
-
-    const addTextDecoreOnChb = () => {
-        const taskItems = document.querySelectorAll('.tasks .task__item');
-        taskItems.forEach((item) => {
-            const todoText = item.querySelector('.task__item__descr');
-            const todoChb = item.querySelector('.realchb');
-            let beforeLisenerObj = geDataFromStorage(toDoObj);
-            onCheckboxClick(todoChb, beforeLisenerObj, todoText);
-
-            todoChb.addEventListener('change', () => {
-                let afterLisenerObj = geDataFromStorage(toDoObj);
-                onCheckboxClick(todoChb, afterLisenerObj, todoText);
-                calcItems();
-            });
-        });
-    }
-    addTextDecoreOnChb();
-
-
+/////////////2)///////////////
     const addNewItem = () => {
         addItem.addEventListener('click', () => {
-            // console.log(todoInput.value);
-            const newData = {
-                id: uniqueId(),
-                text: todoInput.value,
-                read: false
+            const newData = 
+                {
+                    id: uniqueId(),
+                    text: todoInput.value,
+                    read: false
+                };
+            let filtred = geDataFromStorage();
+            if (filtred && Object.keys(filtred).length > 0) {
+                filtred.push(newData); // dobavlyaem novi element
+                // setDataToStorage(filtred);
+                setDataToStorage(filtred);
+                console.log('1');
+            } else {
+                console.log('2');
+                //setDataToStorage(newData);
+                window.localStorage.setItem('toDoObj', JSON.stringify([newData])); // pervaya zapis v storage
             }
-
-            let filtred = geDataFromStorage(toDoObj);
-
-
-            filtred.push(newData); // dobavlyaem novi element
-
-            setDataToStorage(filtred); // zapisivaem v localstorage
-
-            let newArr = geDataFromStorage(toDoObj);
-
-
-            wrightItemsToPage(newArr);
-            addHoverOnTrash();
-             addTextDecoreOnChb();
+            wrightItemsToPage(geDataFromStorage());
             
-            trashItem();
-            calcItems();
+
+            // zapisivaem v localstorage
+            //let newArr = geDataFromStorage();
+
+
+            // wrightItemsToPage(newArr);
+            // addHoverOnTrash();
+            // addTextDecoreOnChb();
+
+            // trashItem();
+            // calcItems();
         });
     }
     addNewItem();
 
 
-    const trashItem = () => {
-        const trashBtns = document.querySelectorAll('.task__item .task__trash');
-        trashBtns.forEach((item) => {
-            item.addEventListener('click', () => {
+    ////////////3)////////////////
+    // const trashItem = () => {
+    //     const trashBtns = document.querySelectorAll('.task__item .task__trash');
+    //     trashBtns.forEach((item) => {
+    //         item.addEventListener('click', () => {
 
-                let trashParent = item.closest('.task__item');
-                let parenId = trashParent.dataset.id;
-                // console.log(parenId);
-
-
-                let storeObj = geDataFromStorage(toDoObj);
-
-                const newArr = storeObj.filter((item) => item.id !== parenId);
-                setDataToStorage(newArr)
-
-                let objfromStorage1 = geDataFromStorage(toDoObj);;
-
-
-                wrightItemsToPage(objfromStorage1);
+    //             let trashParent = item.closest('.task__item');
+    //             let parenId = trashParent.dataset.id;
                 
+    //             let storeObj = geDataFromStorage();
+    //             const newArr = storeObj.filter((item) => item.id !== parenId);
+
+    //             setDataToStorage(newArr)
+    //             let objfromStorage1 = geDataFromStorage();
+    //             wrightItemsToPage(objfromStorage1);
+               
                 
-                trashItem();
-                addHoverOnTrash();
-                addTextDecoreOnChb();
-                calcItems();
-            });
-        });
+               
+    //             // addHoverOnTrash();
+    //             // addTextDecoreOnChb();
+    //             // calcItems();
+    //         });
+            
+    //     });
+        
+    // }
+   // trashItem();
+
+   tasks.addEventListener('click', (event) => {
+    const trashBtn = event.target.closest('.task__trash');
+
+
+    if (trashBtn) {
+        let trashParent = trashBtn.closest('.task__item');
+        let parentId = trashParent.dataset.id;
+        let storeObj = geDataFromStorage();
+        const newArr = storeObj.filter((item) => item.id !== parentId);
+        setDataToStorage(newArr);
+        let objFromStorage = geDataFromStorage();
+        wrightItemsToPage(objFromStorage);
+        
+
+        // Дополнительные функции, если необходимо
+        // addHoverOnTrash();
+        // addTextDecorOnChb();
+        // calcItems();
     }
-    trashItem();
+});
+    
+
+
+
+    /*
+        const onCheckboxClick = (todoChb, objfromStorage1, todoText) => {
+            if (todoChb.checked) {
+                ///////////////////////////////////////////////
+                let taskItem = todoChb.closest('.task__item');
+                let dataId = taskItem.dataset.id;
+                let loop = false;
+                objfromStorage1.forEach((item) => {
+                    if (loop) return;
+                    if (item.id === dataId) {
+                        item.read = true;
+                        loop = true;
+                    }
+                })
+                setDataToStorage(objfromStorage1);
+                if (!todoText.classList.contains('textdecore')) {
+                    todoText.classList.add('textdecore');
+                }
+            } else
+            ///////////////////////////////////////////////
+            {
+                let taskItem = todoChb.closest('.task__item');
+                let dataId = taskItem.dataset.id;
+                let loop = false;
+                objfromStorage1.forEach((item) => {
+                    if (loop) return;
+                    if (item.id === dataId) {
+                        item.read = false;
+                        loop = true;
+                    }
+                })
+                setDataToStorage(objfromStorage1);
+    
+                if (todoText.classList.contains('textdecore')) {
+                    todoText.classList.remove('textdecore');
+                }
+            }
+        }
+    */
+
+
+
+
+
+
+
+
+
+
+    /////////////
+    /*  const calcItems = () => {
+  
+          let obj = geDataFromStorage(toDoObj);
+          tasksCount.textContent = obj.length;
+  
+          let count = 0;
+          obj.forEach((item) => {
+              if (item.read) count += 1;
+          })
+          complatedNum.textContent = `${count} of ${obj.length}`;
+      }
+  
+      calcItems();*/
+    /////////////
+
+    /*
+        const addHoverOnTrash = () => {
+            const trashDecore = document.querySelectorAll('.task__item .task__trash');
+            trashDecore.forEach((item) => {
+                item.addEventListener('mouseenter', () => {
+                    item.classList.add('trashdecore');
+                    setTimeout(() => {
+                        item.classList.remove('trashdecore');
+                    }, 800);
+                });
+            });
+        }
+        addHoverOnTrash();*/
+
+
+
+    /*
+    
+        const addTextDecoreOnChb = () => {
+            const taskItems = document.querySelectorAll('.tasks .task__item');
+            taskItems.forEach((item) => {
+                const todoText = item.querySelector('.task__item__descr');
+                const todoChb = item.querySelector('.realchb');
+                let beforeLisenerObj = geDataFromStorage(toDoObj);
+                onCheckboxClick(todoChb, beforeLisenerObj, todoText);
+    
+                todoChb.addEventListener('change', () => {
+                    let afterLisenerObj = geDataFromStorage(toDoObj);
+                    onCheckboxClick(todoChb, afterLisenerObj, todoText);
+                    calcItems();
+                });
+            });
+        }
+        addTextDecoreOnChb();*/
+
+
+
+
+    
 
 });
