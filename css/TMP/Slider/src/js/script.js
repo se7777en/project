7a9fbe716557440e28d327914c670e298f4db1c8
Melik <melikpapikyan@gmail.com>
2024-03-21 19:40:07 +0400

    -aslider

--- css/TMP/Slider/src/js/script.js
+++ css/TMP/Slider/src/js/script.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
         rightArrow = document.querySelector('.sliderWrapper .slider__rightArrow'),
         slider = document.querySelector('.sliderWrapper .slider'),
         footerBlock = document.querySelector('.sliderWrapper .building-blocks'),
-        footerBlocks = document.querySelectorAll('.sliderWrapper .building-block'),
-        sliderInner = document.querySelector('.sliderWrapper .slider__inner');
+        sliderInner = document.querySelector('.sliderWrapper .slider__inner'),
+        images = document.querySelectorAll('.sliderWrapper .slider__inner img'),
 
+        leftArrowBg = document.querySelector('.sliderWrapper .slider__leftArrow i'),
+        rightArrowBg = document.querySelector('.sliderWrapper .slider__rightArrow i');
 
 
     const Count = 11; // number of image
@@ -28,13 +30,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let slideToshow = 3;
     let gap = 5;
-    let itemWidht = 200 + gap;
+    let itemWidht = 150;
+    let borderRadius = 10;
+    let dots = true;
 
 
+
+    itemWidht = itemWidht + gap;
     slider.style.width = `${itemWidht * slideToshow - gap}px`;
     sliderInner.style.gap = `${gap}px`;
     let shift = 0;
     let sliderCount = sliderInnerItems.length;
+
+    images.forEach((item) => {
+        item.style.width = `${itemWidht - gap}px`; // shirina kajdoi kartinki
+        item.style.borderRadius = `${borderRadius}px`;
+    });
+
 
 
 
@@ -42,11 +54,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // slider.style.width = `${sliderCount * itemWidht}px, overflow: hidden`;
     let forLeft = (sliderCount * itemWidht) - itemWidht * slideToshow;
-    console.log(forLeft);
+    // console.log(forLeft);
     //console.log(forLeft);
 
-    leftArrow.addEventListener('click', () => {
+
+    const leftArrowFunc = () => {
         let footerBlocks = document.querySelectorAll('.sliderWrapper .building-block');
+        // if (shift === 0) { leftArrowBg.classList.add('disabled'); } else {
+        //     leftArrowBg.classList.remove('disabled');
+        // }
         if (shift > -forLeft) {
             const parent = document.querySelector('.sliderWrapper .slider__inner');
             shift -= itemWidht * slideToshow;
@@ -55,36 +71,65 @@ document.addEventListener('DOMContentLoaded', () => {
             //  console.log(Math.abs(((shift / itemWidht) / slideToshow))); //tekushi slaid
             let index = Math.abs(((shift / itemWidht) / slideToshow));
             disableActive(footerBlocks);
-            console.log(index);
+            //console.log(index);
             footerBlocks[index].classList.add('active');
         }
-    });
 
+    }
 
-    rightArrow.addEventListener('click', () => {
+    const rightArrowFunc = () => {
         let footerBlocks = document.querySelectorAll('.sliderWrapper .building-block');
         if (shift < 0) {
+
             //console.log(shift);
             const parent = document.querySelector('.sliderWrapper .slider__inner');
             shift += itemWidht * slideToshow;
-            console.log(Math.abs(((shift / itemWidht) / slideToshow)));
+            //console.log(Math.abs(((shift / itemWidht) / slideToshow)));
             parent.style.transform = `translateX(${shift}px)`;
 
-              //  console.log(Math.abs(((shift / itemWidht) / slideToshow))); //tekushi slaid
-              let index = Math.abs(((shift / itemWidht) / slideToshow));
-              disableActive(footerBlocks);
-              console.log(index);
-              footerBlocks[index].classList.add('active');
+            //  console.log(Math.abs(((shift / itemWidht) / slideToshow))); //tekushi slaid
+            let index = Math.abs(((shift / itemWidht) / slideToshow));
+            disableActive(footerBlocks);
+            footerBlocks[index].classList.add('active');
+        }
+        // else if (shift === 0) {
+        //     rightArrowBg.classList.add('disabled');
+        // }
+    }
+
+    leftArrow.addEventListener('click', () => {
+        leftArrowFunc();
+    });
+
+    rightArrow.addEventListener('click', () => {
+        rightArrowFunc();
+    });
+
+
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === "ArrowLeft") {
+            leftArrowFunc();
+        } else if (event.key === "ArrowRight") {
+            rightArrowFunc();
         }
     });
 
+
+
+
+
     const buildingBlock = () => {
-        let items = '';
-        for (let i = 0; i < Math.floor(sliderCount / slideToshow); i++) {
-           let active = (i === 0) ? 'active' : '';
-            items += `<div class="building-block ${active}"></div>`;
+        if (dots) {
+            let items = '';
+            for (let i = 0; i < Math.floor(sliderCount / slideToshow); i++) {
+                let active = (i === 0) ? 'active' : '';
+                items += `<div class="building-block ${active}"></div>`;
+            }
+            footerBlock.innerHTML = items;
+        }else {
+            footerBlock.innerHTML = '';
         }
-        footerBlock.innerHTML = items;
     }
     buildingBlock();
 
@@ -103,19 +148,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const footerBlocks = document.querySelectorAll('.sliderWrapper .building-block');
         const length = footerBlocks.length;
         footerBlocks.forEach((item, i) => {
+
             item.addEventListener('click', () => {
                 disableActive(footerBlocks);
                 item.classList.add('active');
                 let identify = i * id * slideToshow;
                 let parent = document.querySelector('.sliderWrapper .slider__inner');
-               // if (identify >= -forLeft) {
-                    parent.style.transform = `translateX(${identify}px)`;
-                    shift = identify;
+                // if (identify >= -forLeft) {
+                parent.style.transform = `translateX(${identify}px)`;
+                shift = identify;
                 //} 
             });
         });
 
- 
+
     }
 
     buildingBlockStatus();
@@ -139,39 +185,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //////swaip///////////
 
-    // let startX;
-    // let endX;
-    // let startY;
-    // let endY;
-    // let threshold = 50; // порог для определения свайпа или клика
+    let startX;
+    let endX;
+    let startY;
+    let endY;
+    let threshold = 50; // порог для определения свайпа или клика
 
-    // sliderInner.addEventListener('touchstart', (e) => {
-    //     startX = e.touches[0].clientX;
-    //     startY = e.touches[0].clientY;
-    // });
+    sliderInner.addEventListener('touchstart', (e) => {
+        startX = e.touches[0].clientX;
+        startY = e.touches[0].clientY;
+    });
 
-    // sliderInner.addEventListener('touchmove', (e) => {
-    //     endX = e.touches[0].clientX;
-    //     endY = e.touches[0].clientY;
-    // });
+    sliderInner.addEventListener('touchmove', (e) => {
+        endX = e.touches[0].clientX;
+        endY = e.touches[0].clientY;
+    });
 
-    // sliderInner.addEventListener('touchend', () => {
-    //     const deltaX = startX - endX;
-    //     const deltaY = startY - endY;
-    //     if (Math.abs(deltaX) > threshold || Math.abs(deltaY) > threshold) {
-    //         if (Math.abs(deltaX) > Math.abs(deltaY)) {
-    //             if (deltaX > 50) {
-    //                 // свайп влево
-    //                 scrollToNextSlide();
-    //             } else if (deltaX < -50) {
-    //                 // свайп вправо
-    //                 scrollToPreviousSlide();
-    //             }
-    //         }
-    //     } else {
-    //         // Это был клик, ничего не делаем
-    //     }
-    // });
+    sliderInner.addEventListener('touchend', () => {
+        const deltaX = startX - endX;
+        const deltaY = startY - endY;
+        if (Math.abs(deltaX) > threshold || Math.abs(deltaY) > threshold) {
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                if (deltaX > 50) {
+                    // свайп влево
+
+                    leftArrowFunc();
+                } else if (deltaX < -50) {
+                    // свайп вправо
+                    rightArrowFunc();
+                }
+            }
+        } else {
+            // Это был клик, ничего не делаем
+        }
+    });
 
     // function scrollToNextSlide() {
     //     // Реализуйте логику для прокрутки к следующему слайду
