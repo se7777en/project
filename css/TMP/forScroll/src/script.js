33f2baf33cb56ea32a47dcfd0adab4fef7e10bf3
Melik <melikpapikyan@gmail.com>
2024-03-25 18:07:29 +0400

    -aScroll

--- css/TMP/forScroll/src/script.js
+++ css/TMP/forScroll/src/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const wrapperItem = document.querySelector('.wrapper__item'),
-    mytext = document.querySelector('.mytext');
+    mytext = document.querySelector('.mytext'),
+    uncover = document.querySelector('.block__header-uncover'),
+    blockContent = document.querySelector('.block__content'),
+    faSolidIcon = document.querySelector('.block__header .fa-solid');
 
 
 
@@ -11,7 +14,6 @@ wrapperItem.addEventListener('scroll', () => {
 
     const ScrollPos = wrapperItem.scrollTop;
     const textPos = mytext.offsetTop;
-
     const textHeight = mytext.offsetHeight;
 
 
@@ -20,3 +22,11 @@ wrapperItem.addEventListener('scroll', () => {
     }
 
 });
+
+
+
+
+uncover.addEventListener('click', () => {
+    blockContent.classList.toggle('show');
+    faSolidIcon.classList.toggle('fa-chevron-down');
+})
