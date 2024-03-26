da215d913b1fc027a2c606a11100a0915e6903e8
Melik <melikpapikyan@gmail.com>
2024-03-27 00:23:44 +0400

    -ajs-book

--- css/TMP/js-book/src/js/script.js
+++ css/TMP/js-book/src/js/script.js
@@ -4,28 +4,36 @@ window.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
 
     const helpModal = document.querySelector('.header .help'),
-        headerContact = document.querySelector('.header__contact .header__btn'),
+        headerContact = document.querySelector('.header .header__btn'),
         closeBtn = document.querySelector('.header .help__close');
 
 
-    headerContact.addEventListener('click', () => {
-        helpModal.classList.toggle('show');
+    headerContact.addEventListener('click', (e) => {
+        if(headerContact.contains(e.target)) {
+            helpModal.classList.toggle('show');
+        }
     });
 
-    closeBtn.addEventListener('click', (e) => {
+    closeBtn.addEventListener('click', () => {
         helpModal.classList.remove('show');
-        console.log(e.currentTarget);
+        // console.log(e.currentTarget);      
+    });
 
-        if (e.target !== closeBtn && e.target !== helpModal) {
+
+
+   
+
+    document.addEventListener('click', (e) => {
+        if(!helpModal.contains(e.target) && !headerContact.contains(e.target)){
             helpModal.classList.remove('show');
         }
     });
 
 
     // document.addEventListener('click', (e) => {
-    //     // if(!helpModal.contains(e.target)){
-    //     //     helpModal.classList.remove('show');
-    //     // }
+    //     if (!e.target.closest('#helpModal') && !e.target.closest('.header__btn')) {
+    //         helpModal.classList.remove('show');
+    //     }
     // });
 
 
