ed63c027a2d08c1e3de478920c803b078307d4e3
Melik <melikpapikyan@gmail.com>
2024-04-05 18:59:42 +0400

    -ajs-book

--- css/TMP/js-book/src/js/script.js
+++ css/TMP/js-book/src/js/script.js
@@ -190,28 +190,28 @@ ${item.codeEn}
 
     
 ////////////////////theme colored////////////////////////////////
-    mainBg.addEventListener('click', (e) => {
-        const event = e.target;
-        if (event) {
-            if (event.matches('.wrap__light input')) {
-                obj.bgcolor = 'light';
-                console.log('light'); //
-                setDataToStorage(obj);
-            }
+    // mainBg.addEventListener('click', (e) => {
+    //     const event = e.target;
+    //     if (event) {
+    //         if (event.matches('.wrap__light input')) {
+    //             obj.bgcolor = 'light';
+    //             console.log('light'); //
+    //             setDataToStorage(obj);
+    //         }
 
-            if (event.matches('.wrap__dark input')) {
-                obj.bgcolor = 'dark';
-                console.log('dark'); //
-                setDataToStorage(obj);
-            }
+    //         if (event.matches('.wrap__dark input')) {
+    //             obj.bgcolor = 'dark';
+    //             console.log('dark'); //
+    //             setDataToStorage(obj);
+    //         }
 
-            if (event.matches('.wrap__auto input')) {
-                obj.bgcolor = 'auto';
-                console.log('auto'); //
-                setDataToStorage(obj);
-            }   
-        }
-    });
+    //         if (event.matches('.wrap__auto input')) {
+    //             obj.bgcolor = 'auto';
+    //             console.log('auto'); //
+    //             setDataToStorage(obj);
+    //         }   
+    //     }
+    // });
 
 
 
