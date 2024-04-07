c0ce560419e918ce591337d8d1d2460e69237514
Melik <melikpapikyan@gmail.com>
2024-04-07 17:37:17 +0400

    -atesti

--- css/TMP/tests/js/script.js
+++ css/TMP/tests/js/script.js
@@ -19,7 +19,7 @@ const values = Object.values(obj);
 
 // console.log(questions);
 // console.log(values);
-
+const ind = questions.length;
 
 const ques = document.querySelector('.wrapper__box-title');
 const answs = document.querySelectorAll('.wrapper__box-questions .wrapper__box-item');
@@ -28,6 +28,7 @@ const wrong = document.querySelector('.answers .wrong');
 const right = document.querySelector('.answers .right');
 
 const textarea = document.querySelector('.textarea');
+const indicator = document.querySelector('.indicator');
 // console.log(answs);
 
 
@@ -56,22 +57,23 @@ const addDataToForm = () => {
 addDataToForm();
 
 
-
+let ind2 = ind;
 onlickBtbParent.addEventListener('click', (e) => {
     const event = e.target;
     const rig = e.currentTarget;
     if (event) {
         const item = event.closest('.wrapper__box-question');
         const itemAnsw = item.querySelector('.wrapper__box-item').textContent;
+
         if (itemAnsw === mainAnswer) {
             wrightAnswers += 1;
-            right.textContent = wrightAnswers; 
+            right.textContent = wrightAnswers;
             item.classList.add('right');
             item.querySelector('span').classList.add('right');
         } else {
             wrongAnswers += 1;
             wrong.textContent = wrongAnswers;
-            textarea.value += mainques+' : '+mainAnswer + '\n';
+            textarea.value += mainques + ' : ' + mainAnswer + '\n';
             item.classList.add('wrong');
             item.querySelector('.wrapper__box-item').classList.add('wrong');
             item.querySelector('span').classList.add('wrong');
@@ -84,17 +86,28 @@ onlickBtbParent.addEventListener('click', (e) => {
             }
 
         }
-
-        setTimeout(() => {
-            ['right', 'wrong'].forEach(className => {
-                const elements = document.querySelectorAll('.' + className);
-                elements.forEach(element => {
-                    element.classList.remove(className);
-                });
-            });
-            addDataToForm();
-
-        }, 3000);
-
     }
+
+
+    setTimeout(() => {
+
+        ['right', 'wrong'].forEach(className => {
+            const elements = document.querySelectorAll('.' + className);
+            elements.forEach(element => {
+                element.classList.remove(className);
+            });
+        });
+        addDataToForm();
+
+        ind2--;
+        let val = Math.abs(Math.floor(ind2 * 100 / ind) - 100);
+        if(val >= 100) {
+            val = 0;
+            ind2 = ind;
+        }
+        indicator.style.setProperty('--width', `${val}%`);
+
+    }, 3000);
 });
+
+
