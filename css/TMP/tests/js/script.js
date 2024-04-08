7c5a8623dd610969499b787041833b12d4b32aea
Melik <melikpapikyan@gmail.com>
2024-04-08 09:31:27 +0400

    -atesti

--- css/TMP/tests/js/script.js
+++ css/TMP/tests/js/script.js
@@ -2,101 +2,106 @@
 window.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
 
-const questions = Object.keys(obj);
-const values = Object.values(obj);
+    const questions = Object.keys(obj);
+    const values = Object.values(obj);
 
-// console.log(questions);
-// console.log(values);
-const ind = questions.length;
+    // console.log(questions);
+    // console.log(values);
+    const ind = questions.length;
 
-const ques = document.querySelector('.wrapper__box-title');
-const answs = document.querySelectorAll('.wrapper__box-questions .wrapper__box-item');
-const onlickBtbParent = document.querySelector('.wrapper__box-questions');
-const wrong = document.querySelector('.answers .wrong');
-const right = document.querySelector('.answers .right');
+    const ques = document.querySelector('.wrapper__box-title');
+    const answs = document.querySelectorAll('.wrapper__box-questions .wrapper__box-item');
+    const onlickBtbParent = document.querySelector('.wrapper__box-questions');
+    const wrong = document.querySelector('.answers .wrong');
+    const right = document.querySelector('.answers .right');
 
-const textarea = document.querySelector('.textarea');
-const indicator = document.querySelector('.indicator');
-// console.log(answs);
-
-
-
-let rndRight = 0;
-let mainAnswer = '';
-let mainques = '';
-
-let wrightAnswers = 0;
-let wrongAnswers = 0;
-const addDataToForm = () => {
-    let randomIndex = Math.floor(Math.random() * values.length);
-    mainques = questions[randomIndex]; // vopros
-    mainAnswer = obj[mainques]; // otvet
-    ques.textContent = `${mainques}`;
-
-    answs.forEach((item) => { // randomnie otveti
-        let random = Math.floor(Math.random() * values.length);
-        item.textContent = values[random];
-    });
-
-    rndRight = Math.floor(Math.random() * 4);
-    answs[rndRight].textContent = mainAnswer; // vstavlyaem pravilni otvet
-}
-
-addDataToForm();
+    const textarea = document.querySelector('.textarea');
+    const indicator = document.querySelector('.indicator');
+    // console.log(answs);
 
 
-let ind2 = ind;
-onlickBtbParent.addEventListener('click', (e) => {
-    const event = e.target;
-    const rig = e.currentTarget;
-    if (event) {
-        const item = event.closest('.wrapper__box-question');
-        const itemAnsw = item.querySelector('.wrapper__box-item').textContent;
 
-        if (itemAnsw === mainAnswer) {
-            wrightAnswers += 1;
-            right.textContent = wrightAnswers;
-            item.classList.add('right');
-            item.querySelector('span').classList.add('right');
-        } else {
-            wrongAnswers += 1;
-            wrong.textContent = wrongAnswers;
-            textarea.value += mainques + ' : ' + mainAnswer + '\n';
-            item.classList.add('wrong');
-            item.querySelector('.wrapper__box-item').classList.add('wrong');
-            item.querySelector('span').classList.add('wrong');
+    let rndRight = 0;
+    let mainAnswer = '';
+    let mainques = '';
 
+    let wrightAnswers = 0;
+    let wrongAnswers = 0;
+    const addDataToForm = () => {
+        let randomIndex = Math.floor(Math.random() * values.length);
+        mainques = questions[randomIndex]; // vopros
+        mainAnswer = obj[mainques]; // otvet
+        ques.textContent = `${mainques}`;
 
-            const answ = rig.querySelector(`[data-id="${rndRight + 1}"]`);
-            if (answ) {
-                answ.classList.add('right');
-                answ.querySelector('span').classList.add('right');
-            }
+        answs.forEach((item) => { // randomnie otveti
+            let random = Math.floor(Math.random() * values.length);
+            item.textContent = values[random];
+        });
 
-        }
+        rndRight = Math.floor(Math.random() * 4);
+        answs[rndRight].textContent = mainAnswer; // vstavlyaem pravilni otvet
     }
 
+    addDataToForm();
 
-    setTimeout(() => {
+let clicked = true;
+    let ind2 = ind;
+    onlickBtbParent.addEventListener('click', (e) => {
+        if(clicked === true){
+        const event = e.target;
+        const rig = e.currentTarget;
+        if (event) {
+            const item = event.closest('.wrapper__box-question');
+            const itemAnsw = item.querySelector('.wrapper__box-item').textContent;
 
-        ['right', 'wrong'].forEach(className => {
-            const elements = document.querySelectorAll('.' + className);
-            elements.forEach(element => {
-                element.classList.remove(className);
-            });
-        });
-        addDataToForm();
+            if (itemAnsw === mainAnswer) {
+                wrightAnswers += 1;
+                right.textContent = wrightAnswers;
+                item.classList.add('right');
+                item.querySelector('span').classList.add('right');
+            } else {
+                wrongAnswers += 1;
+                wrong.textContent = wrongAnswers;
+                textarea.value += mainques + ' : ' + mainAnswer + '\n';
+                item.classList.add('wrong');
+                item.querySelector('.wrapper__box-item').classList.add('wrong');
+                item.querySelector('span').classList.add('wrong');
 
-        ind2--;
-        let val = Math.abs(Math.floor(ind2 * 100 / ind) - 100);
-        if(val >= 100) {
-            val = 0;
-            ind2 = ind;
+
+                const answ = rig.querySelector(`[data-id="${rndRight + 1}"]`);
+                if (answ) {
+                    answ.classList.add('right');
+                    answ.querySelector('span').classList.add('right');
+                }
+
+            }
         }
-        indicator.style.setProperty('--width', `${val}%`);
 
-    }, 3000);
-});
+    }
+
+    clicked = false;
+
+
+        setTimeout(() => {
+
+            ['right', 'wrong'].forEach(className => {
+                const elements = document.querySelectorAll('.' + className);
+                elements.forEach(element => {
+                    element.classList.remove(className);
+                });
+            });
+            addDataToForm();
+
+            ind2--;
+            let val = Math.abs(Math.floor(ind2 * 100 / ind) - 100);
+            if (val >= 100) {
+                val = 0;
+                ind2 = ind;
+            }
+            indicator.style.setProperty('--width', `${val}%`);
+            clicked = true;
+        }, 3000);
+    });
 
 
 });
