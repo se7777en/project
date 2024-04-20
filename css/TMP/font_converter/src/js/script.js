e45f492ff86ff681721a37979b50a0b96c579a7c
Melik <melikpapikyan@gmail.com>
2024-04-21 00:58:15 +0400

    -aFont_converter

--- css/TMP/font_converter/src/js/script.js
+++ css/TMP/font_converter/src/js/script.js
@@ -58,13 +58,14 @@ document.addEventListener('DOMContentLoaded', (e) => {
         });
 
         wordsCount.textContent = wordsLength.length; 
-        console.log(wordsLength);
+       // console.log(wordsLength);
 
         let englishWordCount = words.filter(word => /^[a-zA-Z-_:.,\d]+$/.test(word)).length;
-        let georgianWordCount = words.filter(word => /^[ა-ჰ\d\-_:.,]+$/u.test(word)).length;
-        let containsLatinParliament = words.filter(word => /[\u00C0-\u00FF-_:.,\d]+/.test(word)).length;
+        let georgianWordCount = words.filter(word => /^[ა-ჰ\d\-_:.,\d]+$/u.test(word)).length;
+        //let containsLatinParliament = words.filter(word => /[\u00C0-\u00FF\d -_:.,]+/.test(word)).length;
+        let containsLatinParliament = words.filter(word => /^[\u00C0-\u00FF\d -_:.,]+$/u.test(word)).length;
         //let containsAsomtavruli = words.filter(word => /[\u10A0-\u10FF\u{10A0E}-\u{10AEF}]/u.test(word)).length;
-        let containsAsomtavruli = words.filter(word => /^[\u10A0-\u10FF\u1C90-\u1CBA _\-:.,]+$/u.test(word)).length;
+        let containsAsomtavruli = words.filter(word => /^[\u10A0-\u10FF\d -_:.,]+$/u.test(word)).length;
 
         
 
