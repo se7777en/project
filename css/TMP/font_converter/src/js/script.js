690bf7daa3b7840c8464f60ced3402c830106ea4
Melik <melikpapikyan@gmail.com>
2024-04-21 19:19:45 +0400

    -aFont_converter

--- css/TMP/font_converter/src/js/script.js
+++ css/TMP/font_converter/src/js/script.js
@@ -65,9 +65,9 @@ document.addEventListener('DOMContentLoaded', (e) => {
                 let englishWordCount = (words.filter(word => /^[a-zA-Z-_:.,\d]+$/.test(word)).length);
                 let georgianWordCount = (words.filter(word => /^[ა-ჰ\d\-_:.,\d]+$/u.test(word)).length);
                 //let containsLatinParliament = words.filter(word => /[\u00C0-\u00FF\d -_:.,]+/.test(word)).length;
-                let containsLatinParliament = (words.filter(word => /^[\u00C0-\u00FF\d -_:.,]+$/u.test(word)).length);
+                let containsLatinParliament = (words.filter(word => /^[\u00C0-\u00FF -_:.,\d]+$/u.test(word)).length);
                 //let containsAsomtavruli = words.filter(word => /[\u10A0-\u10FF\u{10A0E}-\u{10AEF}]/u.test(word)).length;
-                let containsAsomtavruli = (words.filter(word => /^[\u10A0-\u10FF\d -_:.,]+$/u.test(word)).length);
+                let containsAsomtavruli = (words.filter(word => /^[\u10A0-\u10FF-_:.,\d]+$/u.test(word)).length);
 
 
 
