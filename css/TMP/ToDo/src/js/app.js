bd3163eafe7676cd26912b285a47ed7835151d90
Melik <melikpapikyan@gmail.com>
2024-04-22 16:24:40 +0400

    -atodo

--- css/TMP/ToDo/src/js/app.js
+++ css/TMP/ToDo/src/js/app.js
@@ -163,6 +163,10 @@ window.addEventListener('DOMContentLoaded', (e) => {
         tasks.addEventListener('click', (event) => {
             const trashBtn = event.target.closest('.task__trash');
 
+            trashBtn.addEventListener('mousedown', function (e) {
+                e.preventDefault();
+            });
+
             if (trashBtn) {
                 let trashParent = trashBtn.closest('.task__item');
                 let parentId = trashParent.dataset.id;
