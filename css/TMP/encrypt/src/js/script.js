8782c95aa8b8053396e8dcd5f6a2a53d9f060848
Melik <melikpapikyan@gmail.com>
2024-04-28 11:41:40 +0400

    -apassEncrypt

--- css/TMP/encrypt/src/js/script.js
+++ css/TMP/encrypt/src/js/script.js
@@ -2,99 +2,106 @@
 
 document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
+    let currentUrl = window.location.href;
 
-    const textArea = document.querySelector('.textarea'),
-        encrypt = document.querySelector('.wrapper__btns .encrypt'),
-        decrypt = document.querySelector('.wrapper__btns .decrypt'),
-        copyWraper = document.querySelector('.wrapper__text .copy'),
-        copyIcon = document.querySelector('.wrapper__text .copy i');
+    if (currentUrl.indexOf('secure-crypt.surge.sh') === -1) {
+        if (currentUrl.indexOf('secure-crypt.surge.sh') === -1) {
+            if (currentUrl.indexOf('secure-crypt.surge.sh') === -1) {
 
-
-
-    copyWraper.addEventListener('click', () => {
-        copyIcon.classList.remove('fa-copy');
-        copyIcon.classList.add('fa-check');
-        copyWraper.classList.add('colored');
-        const areatext = textArea.value;
-        if (areatext.length > 0) {
-            navigator.clipboard.writeText(areatext);
-            setTimeout(() => {
-                copyIcon.classList.add('fa-copy');
-                copyIcon.classList.remove('fa-check');
-                copyWraper.classList.remove('colored');
-            }, 1000);
-        }
-    });
-
-    textArea.addEventListener('input', () => {
-        if (textArea.value.length > 0) {
-            copyWraper.classList.add('show');
-        } else {
-            copyWraper.classList.remove('show');
-        }
-    });
+                const textArea = document.querySelector('.textarea'),
+                    encrypt = document.querySelector('.wrapper__btns .encrypt'),
+                    decrypt = document.querySelector('.wrapper__btns .decrypt'),
+                    copyWraper = document.querySelector('.wrapper__text .copy'),
+                    copyIcon = document.querySelector('.wrapper__text .copy i');
 
 
 
-    function encryptPassword(password, textToEncrypt) {
-        // Используем AES для шифрования пароля
-        const encrypted = CryptoJS.AES.encrypt(textToEncrypt, password).toString();
-        return encrypted;
-    }
+                copyWraper.addEventListener('click', () => {
+                    copyIcon.classList.remove('fa-copy');
+                    copyIcon.classList.add('fa-check');
+                    copyWraper.classList.add('colored');
+                    const areatext = textArea.value;
+                    if (areatext.length > 0) {
+                        navigator.clipboard.writeText(areatext);
+                        setTimeout(() => {
+                            copyIcon.classList.add('fa-copy');
+                            copyIcon.classList.remove('fa-check');
+                            copyWraper.classList.remove('colored');
+                        }, 1000);
+                    }
+                });
 
-    function decryptText(password, encryptedText) {
-        try {
-            // Расшифровываем текст с использованием AES и пароля
-            const decrypted = CryptoJS.AES.decrypt(encryptedText, password).toString(CryptoJS.enc.Utf8);
-            return decrypted;
-        } catch (error) {
-            // Если возникла ошибка при расшифровке, возвращаем null
-            console.error("Decryption error:", error.message);
-            return null;
-        }
-    }
-
-
-    encrypt.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
-    });
-
-
-    encrypt.addEventListener('click', (e) => {
-        e.preventDefault();
-        const pass = document.querySelector('.wrapper__inputs input').value;
-        document.querySelector('.wrapper__inputs input').value = '';
-        const text = textArea.value;
-        if (text.length > 0) {
-            textArea.value = '';
-            textArea.value = encryptPassword(pass, text);
-            //console.log(textArea.value);
-        }
-    });
+                textArea.addEventListener('input', () => {
+                    if (textArea.value.length > 0) {
+                        copyWraper.classList.add('show');
+                    } else {
+                        copyWraper.classList.remove('show');
+                    }
+                });
 
 
-    decrypt.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
-    });
 
-    decrypt.addEventListener('click', (e) => {
-        e.preventDefault();
-        const pass = document.querySelector('.wrapper__inputs input').value;
-        document.querySelector('.wrapper__inputs input').value = '';
-        const text = textArea.value;
-        if (text.length > 0) {
-            textArea.value = '';
-            let decrypttext = decryptText(pass, text);
-            if (decrypttext) {
-                textArea.value = decrypttext;
-            } else {
-                textArea.placeholder = 'Enter the correct password';
-            }
+                function encryptPassword(password, textToEncrypt) {
+                    const encrypted = CryptoJS.AES.encrypt(textToEncrypt, password).toString();
+                    return encrypted;
+                };
 
-            //  console.log(decrypttext.length);
+                function decryptText(password, encryptedText) {
+                    try {
+                        const decrypted = CryptoJS.AES.decrypt(encryptedText, password).toString(CryptoJS.enc.Utf8);
+                        return decrypted;
+                    } catch (error) {
+                        console.error("Decryption error:", error.message);
+                        return null;
+                    }
+                };
 
-        }
 
-        // console.log(textArea.value);
-    });
+                encrypt.addEventListener('contextmenu', (e) => {
+                    e.preventDefault();
+                });
+
+
+                encrypt.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    const pass = document.querySelector('.wrapper__inputs input').value;
+                    document.querySelector('.wrapper__inputs input').value = '';
+                    const text = textArea.value;
+                    if (text.length > 0) {
+                        textArea.value = '';
+                        textArea.value = encryptPassword(pass, text);
+                        //console.log(textArea.value);
+                    }
+                });
+
+
+                decrypt.addEventListener('contextmenu', (e) => {
+                    e.preventDefault();
+                });
+
+                decrypt.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    const pass = document.querySelector('.wrapper__inputs input').value;
+                    document.querySelector('.wrapper__inputs input').value = '';
+                    const text = textArea.value;
+                    if (text.length > 0) {
+                        textArea.value = '';
+                        let decrypttext = decryptText(pass, text);
+                        if (decrypttext) {
+                            textArea.value = decrypttext;
+                        } else {
+                            textArea.placeholder = 'Enter the correct password';
+                        }
+
+                        //  console.log(decrypttext.length);
+
+                    }
+
+                    // console.log(textArea.value);
+                });
+
+            };
+        };
+    };
+
 });
