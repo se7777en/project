1edd6c8d9b7f06a8d9cf863485981025a8a46bf4
Melik <melikpapikyan@gmail.com>
2024-05-05 12:30:25 +0400

    -aColorPicker

--- css/TMP/colorPicker/src/js/app.js
+++ css/TMP/colorPicker/src/js/app.js
@@ -148,7 +148,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
         if (item) {
             lockId = item.dataset.id;
             item.dataset.id = lockId === '0' ? '1' : '0';
-           
+
             const sec = item.querySelector('.fa-solid');
             sec.classList.toggle('fa-lock-open');
             sec.classList.toggle('fa-lock');
@@ -268,15 +268,87 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
     //let intensity = getColorIntensity(randomColor);
 
+    //////////////////////////////////drag drop//////////////////////////////////////////////////////
+
+
+    /////////1////////for mobile/////
+    const tasksListElement = document.querySelector(`.main`);
+    const taskElements = tasksListElement.querySelectorAll(`.main__item`);
+
+    for (const task of taskElements) {
+        task.draggable = true;
+    }
+    /////////1////////////
+
+    /////////2////////////
+    tasksListElement.addEventListener(`touchstart`, (evt) => {
+        evt.target.classList.add(`selected`);
+    });
+
+    tasksListElement.addEventListener(`touchend`, (evt) => {
+        evt.target.classList.remove(`selected`);
+    });
+    /////////2////////////
+
+    /////////3////////////
+    tasksListElement.addEventListener(`touchmove`, (evt) => {
+        evt.preventDefault();
+
+        const activeElement = tasksListElement.querySelector(`.selected`);
+        const currentElement = document.elementFromPoint(evt.touches[0].clientX, evt.touches[0].clientY);
+        const isMoveable = activeElement !== currentElement &&
+            currentElement.classList.contains(`main__item`);
+
+        if (!isMoveable) {
+            return;
+        }
+
+        const nextElement = (currentElement === activeElement.nextElementSibling) ?
+            currentElement.nextElementSibling :
+            currentElement;
+
+        tasksListElement.insertBefore(activeElement, nextElement);
+    });
+    /////////3////////////
+
+    /////////4////////////
+    const getNextElement = (cursorPosition, currentElement) => {
+        const currentElementCoord = currentElement.getBoundingClientRect();
+        const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+        const nextElement = (cursorPosition < currentElementCenter) ?
+            currentElement :
+            currentElement.nextElementSibling;
+
+        return nextElement;
+    };
+    /////////4////////////
+
+    /////////5////////////
+    tasksListElement.addEventListener(`touchmove`, (evt) => {
+        evt.preventDefault();
+
+        const activeElement = tasksListElement.querySelector(`.selected`);
+        const currentElement = document.elementFromPoint(evt.touches[0].clientX, evt.touches[0].clientY);
+        const isMoveable = activeElement !== currentElement &&
+            currentElement.classList.contains(`main__item`);
+
+        if (!isMoveable) {
+            return;
+        }
+
+        const nextElement = getNextElement(evt.touches[0].clientY, currentElement);
+
+        if (
+            nextElement &&
+            activeElement === nextElement.previousElementSibling ||
+            activeElement === nextElement) {
+            return;
+        }
+
+        tasksListElement.insertBefore(activeElement, nextElement);
+    });
 
 
 });
-
-
-
-
-
-
-
-
-
+/////////5////////////
