30f7e512ba9e85b1e766cadbb4bf5d1861842ee0
Melik <melikpapikyan@gmail.com>
2024-05-05 12:50:25 +0400

    -aColorPicker

--- css/TMP/colorPicker/src/js/app.js
+++ css/TMP/colorPicker/src/js/app.js
@@ -269,9 +269,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     //let intensity = getColorIntensity(randomColor);
 
     //////////////////////////////////drag drop//////////////////////////////////////////////////////
-
-
-    /////////1////////for mobile/////
+    
     const tasksListElement = document.querySelector(`.main`);
     const taskElements = tasksListElement.querySelectorAll(`.main__item`);
     
@@ -314,6 +312,13 @@ document.addEventListener('DOMContentLoaded', (e) => {
             return;
         }
     
+        // Добавляем плавное перемещение
+        activeElement.style.transition = 'transform 0.2s ease-in-out';
+        activeElement.style.transform = 'translateY(10px)'; // Можете изменить значение для адаптации
+    
+        // Добавляем плавное изменение цвета
+        activeElement.style.backgroundColor = 'lightgray';
+    
         tasksListElement.insertBefore(activeElement, nextElement);
     });
     
@@ -328,6 +333,15 @@ document.addEventListener('DOMContentLoaded', (e) => {
         return nextElement;
     };
     
+
+
+
+
+
+
+
+
+
 
 
 });
