0209ca2e7e1adfd2ba5a30d4cbac9e607f656c3f
Melik <melikpapikyan@gmail.com>
2024-05-05 13:46:44 +0400

    -aColorPicker

--- css/TMP/colorPicker/src/js/app.js
+++ css/TMP/colorPicker/src/js/app.js
@@ -269,71 +269,74 @@ document.addEventListener('DOMContentLoaded', (e) => {
     //let intensity = getColorIntensity(randomColor);
 
     //////////////////////////////////drag drop//////////////////////////////////////////////////////
-    
-   
 
-    /////////1////////for mobile/////
-    const tasksListElement = document.querySelector(`.main`);
-    const taskElements = tasksListElement.querySelectorAll(`.main__item`);
-    
-    for (const task of taskElements) {
-        task.draggable = true;
+    const tasksListElement = document.querySelector('.main');
+    const taskElements = tasksListElement.querySelectorAll('.main__item');
+    let activeTask = null;
+    if (window.innerWidth < 980) {
+        for (const task of taskElements) {
+            task.draggable = true;
+        }
+
+        tasksListElement.addEventListener('touchmove', (evt) => {
+            evt.preventDefault();
+
+            const activeElement = tasksListElement.querySelector('.selected');
+            const currentElement = document.elementFromPoint(evt.touches[0].clientX, evt.touches[0].clientY);
+
+            // Проверяем, что текущий элемент не null и является дочерним элементом tasksListElement
+            if (!currentElement || !tasksListElement.contains(currentElement)) {
+                return;
+            }
+
+            const isMoveable = activeElement !== currentElement &&
+                currentElement.classList.contains('main__item');
+
+            if (!isMoveable) {
+                return;
+            }
+
+            const nextElement = getNextElement(evt.touches[0].clientY, currentElement);
+
+            if (nextElement &&
+                (activeElement === nextElement.previousElementSibling ||
+                    activeElement === nextElement)) {
+                return;
+            }
+
+            tasksListElement.insertBefore(activeElement, nextElement);
+        });
+
+        tasksListElement.addEventListener('touchstart', (evt) => {
+            const targetTask = evt.target.closest('.main__item');
+            if (targetTask) {
+                activeTask = {
+                    element: targetTask,
+                    startX: evt.touches[0].clientX,
+                    startY: evt.touches[0].clientY
+                };
+                targetTask.classList.add('selected');
+            }
+        });
+
+        tasksListElement.addEventListener('touchend', () => {
+            if (activeTask) {
+                activeTask.element.classList.remove('selected');
+                activeTask = null;
+            }
+        });
+
+        const getNextElement = (cursorPosition, currentElement) => {
+            const currentElementCoord = currentElement.getBoundingClientRect();
+            const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+            const nextElement = (cursorPosition < currentElementCenter) ?
+                currentElement :
+                currentElement.nextElementSibling;
+
+            return nextElement;
+        };
     }
-    
-    tasksListElement.addEventListener(`touchstart`, (evt) => {
-        const targetTask = evt.target.closest('.main__item');
-        if (targetTask) {
-            targetTask.classList.add(`selected`);
-        }
-    });
-    
-    tasksListElement.addEventListener(`touchend`, (evt) => {
-        const selectedTask = tasksListElement.querySelector('.main__item.selected');
-        if (selectedTask) {
-            selectedTask.classList.remove(`selected`);
-        }
-    });
-    
-    tasksListElement.addEventListener(`touchmove`, (evt) => {
-        evt.preventDefault();
-    
-        const activeElement = tasksListElement.querySelector(`.selected`);
-        const currentElement = document.elementFromPoint(evt.touches[0].clientX, evt.touches[0].clientY);
-        const isMoveable = activeElement !== currentElement &&
-            currentElement.classList.contains(`main__item`);
-    
-        if (!isMoveable) {
-            return;
-        }
-    
-        const nextElement = getNextElement(evt.touches[0].clientY, currentElement);
-    
-        if (
-            nextElement &&
-            activeElement === nextElement.previousElementSibling ||
-            activeElement === nextElement) {
-            return;
-        }
-    
-        tasksListElement.insertBefore(activeElement, nextElement);
-    });
-    
-    const getNextElement = (cursorPosition, currentElement) => {
-        const currentElementCoord = currentElement.getBoundingClientRect();
-        const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
-    
-        const nextElement = (cursorPosition < currentElementCenter) ?
-            currentElement :
-            currentElement.nextElementSibling;
-    
-        return nextElement;
-    };
-    
-
-
-
-
-
 
 
 
