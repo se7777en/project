bcd93bbeea96a3728b5741b6de6efc526d222335
Melik <melikpapikyan@gmail.com>
2024-05-22 16:51:24 +0400

    -amap_ofGeo

--- css/TMP/ajaraMap/src/js/script.js
+++ css/TMP/ajaraMap/src/js/script.js
@@ -1,21 +1,21 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', (e) => {
-    e.preventDefault();
+// document.addEventListener('DOMContentLoaded', (e) => {
+//     e.preventDefault();
 
-    const kartli = document.querySelector('.kartli');
-    const border = document.querySelectorAll('.border');
+//     const kartli = document.querySelector('.kartli');
+//     const border = document.querySelectorAll('.border');
 
-    kartli.addEventListener('click', () => {
-        kartli.classList.add('scal');
+//     kartli.addEventListener('click', () => {
+//         kartli.classList.add('scal');
 
-        // border.forEach((item) => {
-        //     item.classList.add('index');
-        // });
-        setTimeout(() => {
-            kartli.classList.remove('scal');
-            // border.forEach((item) => {
-            //     item.classList.remove('index');
-            // });
-        }, 3000);
-    });
-});
+//         // border.forEach((item) => {
+//         //     item.classList.add('index');
+//         // });
+//         setTimeout(() => {
+//             kartli.classList.remove('scal');
+//             // border.forEach((item) => {
+//             //     item.classList.remove('index');
+//             // });
+//         }, 3000);
+//     });
+// });
