5bc494c8df5b7018e99c436b3b485baaf7ec622e
Melik <melikpapikyan@gmail.com>
2024-05-23 21:27:10 +0400

    -ageoMap

--- css/TMP/GeorgiaMap/src/js/script.js
+++ css/TMP/GeorgiaMap/src/js/script.js
@@ -2,31 +2,61 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
 
-    const items = document.querySelectorAll('.item');
+   // const items = document.querySelectorAll('.item');
     const activeItems = document.querySelector('.active-items');
-    const borders = document.querySelector('.borders');
+   // const borders = document.querySelector('.borders');
     // console.log(items);
+
+    activeItems.addEventListener('click', (e) => {
+        const event = e.target;
+        const closestItem = event.closest('.item');
+        if (closestItem && event) {
+            if (!closestItem.matches('.scal')) {
+
+                closestItem.querySelectorAll('.municipal').forEach((item)=>{
+                    item.classList.add('stroke');
+                });
+                
+                //console.log('ok');
+                activeItems.appendChild(closestItem);
+                closestItem.classList.add('scal');
+            }else {
+                activeItems.prepend(closestItem);
+                closestItem.classList.remove('scal');
+
+                closestItem.querySelectorAll('.municipal').forEach((item)=>{
+                    item.classList.remove('stroke');
+                });
+            }
+        }
+    });
 
     // const border = document.querySelectorAll('.border');
 
-    items.forEach((item) => {
-        item.addEventListener('click', () => {
-            activeItems.appendChild(item);
-            item.classList.toggle('scal');
+    // items.forEach((item) => {
+    //     item.addEventListener('click', () => {
+    //         if (!item.matches('.scal')) {
+    //         activeItems.appendChild(item);
+    //         item.classList.toggle('scal');
+    //         activeItems.insertBefore(borders, item);
+    //         //activeItems.prepend(borders);
 
-            if (!item.matches('.scal')) {
-                activeItems.appendChild(borders);
-            } else {
-                activeItems.prepend(borders);
-            }
+    //            // activeItems.appendChild(borders);
+    //         } else {
+    //            // activeItems.prepend(item);
 
-            // setTimeout(() => {
-            //     item.classList.remove('scal');
-            //     activeItems.appendChild(borders);
-            // }, 3000);
 
-        });
-    });
+    //              item.classList.toggle('scal');
+    //              activeItems.parentNode.insertAdjacentElement('afterend', borders);
+    //          }
+
+    //         // setTimeout(() => {
+    //         //     item.classList.remove('scal');
+    //         //     activeItems.appendChild(borders);
+    //         // }, 3000);
+
+    //     });
+    // });
 
     // kartli.addEventListener('click', () => {
     //     kartli.classList.add('scal');
