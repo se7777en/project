0efd83bb322f56aa2bc7ff75b595ceef7156c41a
Melik <melikpapikyan@gmail.com>
2024-05-25 15:59:56 +0400

    -ageoMap

--- css/TMP/GeorgiaMap/src/js/script.js
+++ css/TMP/GeorgiaMap/src/js/script.js
@@ -4,8 +4,76 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
    // const items = document.querySelectorAll('.item');
     const activeItems = document.querySelector('.active-items');
+    const modal = document.querySelector('.modal');
+    const modalClose = document.querySelector('.modal__close');
+    const modalImg = document.querySelector('.modal__img');
+    const modalDescr = document.querySelector('.modal__descr');
+    const location = document.querySelector('.location a');
    // const borders = document.querySelector('.borders');
     // console.log(items);
+
+    let activePoint = '';
+
+    modalClose.addEventListener('click', (e) => {
+        e.preventDefault();
+        modal.classList.remove('active');
+        activePoint.classList.remove('selected');
+    });
+
+    const dataObj = {
+      '01': {
+        'text': '1Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '02': {
+        'text': '2Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '03': {
+        'text': '3Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '04': {
+        'text': '3Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '05': {
+        'text': '4Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '06': {
+        'text': '5Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '07': {
+        'text': '6Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '08': {
+        'text': 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '09': {
+        'text': 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68'
+      },
+      '10': {
+        'text': 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'img': './img/bilberry.jpeg',
+        'location': 'https://maps.app.goo.gl/Km29RGUYk9MZYEk68@@'
+      },
+    }
+
+    
 
     activeItems.addEventListener('click', (e) => {
         const event = e.target;
@@ -33,10 +101,42 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
         if (event && closestItem && event.closest('.point')) {
            // console.log(event);
+           modal.classList.remove('active');
+           modal.classList.remove('anime'); 
            if(event.dataset && event.dataset.info){
-            console.log(event.dataset.info);
-           }
+            activePoint = event;
+           // console.log(event.dataset.info);
+            const data  = event.dataset.info;
+
+            if(dataObj[data]) {
+                console.log(dataObj[data]);
+                modalImg.src = dataObj[data].img;
+                modalDescr.textContent = dataObj[data].text;
+                location.href = dataObj[data].location;
+            }
+
+            
+
+            
+            activeItems.querySelectorAll('.selected').forEach((item) => {
+                item.classList.remove('selected');
+            });
            
+
+
+           
+            modal.classList.add('active');    
+            event.classList.add('selected');
+
+
+            modal.classList.add('anime');
+            
+            setTimeout(() => {
+                modal.classList.remove('anime');  
+            }, 1000);
+
+
+           } 
         }
     });
 
