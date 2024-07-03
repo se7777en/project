13f12b90de0db6b0522c994697aaa46efa21a5fb
Melik <melikpapikyan@gmail.com>
2024-07-03 15:25:25 +0400

    -aportfolio

--- css/TMP/CV/potfolio/src/js/script.js
+++ css/TMP/CV/potfolio/src/js/script.js
@@ -10,11 +10,33 @@
 'use strict';
 
 const active = document.querySelector('.header__burger'),
-      headerMenu = document.querySelector('.header__menu'),
-      body = document.querySelector('body');
+  headerMenu = document.querySelector('.header__menu'),
+  body = document.querySelector('body');
 
-      active.addEventListener('click', () => {
-        active.classList.toggle('active');
-        headerMenu.classList.toggle('active');
-        body.classList.toggle('lock');
-      });
+active.addEventListener('click', () => {
+  active.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  body.classList.toggle('lock');
+});
+
+
+
+// function downloadResumeAsPDF() {
+//   const element = document.body; // Выбираем весь HTML документ
+
+//   // Настройки PDF
+//   const options = {
+//       filename: 'resume.pdf',
+//       html2canvas: { scale: 2 }, // масштаб для html2canvas
+//       jsPDF: {
+//           format: [350, 297], // Размер страницы A4 в миллиметрах [ширина, высота]
+//           orientation: 'portrait', // Ориентация страницы (landscape - альбомная)
+//           unit: 'mm' // Единица измерения миллиметры
+//       }
+//   };
+
+//   // Создаем PDF
+//   html2pdf().set(options).from(element).save();
+// }
+
+// document.getElementById('downloadButton').addEventListener('click', downloadResumeAsPDF);
