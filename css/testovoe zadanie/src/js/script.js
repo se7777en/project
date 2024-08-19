553ac1d4ae400b51c759a89f46969918a937f93b
Melik <melikpapikyan@gmail.com>
2024-08-20 00:26:36 +0400

    -atestovoe-zadanie

--- css/testovoe zadanie/src/js/script.js
+++ css/testovoe zadanie/src/js/script.js
@@ -1,13 +1,18 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
-    function formatPhone(input) {
-        let value = input.value.replace(/\D/g, ''); // Удаляем все нецифровые символы
-        if (value.length > 0) value = '+7 (' + value;
-        if (value.length > 6) value = value.slice(0, 6) + ') ' + value.slice(6);
-        if (value.length > 11) value = value.slice(0, 11) + '-' + value.slice(11);
-        if (value.length > 14) value = value.slice(0, 14) + '-' + value.slice(14);
-        input.value = value.slice(0, 18); // Обрезаем до максимальной длины
-    }
+
 
 });
+
+
+
+/*
+document.querySelector('.scroll-container').addEventListener('scroll', function() {
+  if (this.scrollTop > 50) { // Если прокрутили больше, чем на 50px
+    this.classList.add('scrolled');
+  } else {
+    this.classList.remove('scrolled');
+  }
+});
+*/
