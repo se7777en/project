29e1d2cbf1a16ce4b49ffc6ea191c2c02a2d8ac6
Melik <melikpapikyan@gmail.com>
2024-08-20 17:21:02 +0400

    -atestovoe-zadanie

--- css/testovoe zadanie/src/js/script.js
+++ css/testovoe zadanie/src/js/script.js
@@ -1,6 +1,26 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', (e) => {
-    e.preventDefault();
+  e.preventDefault();
+
+  const burger = document.querySelector('.burger__menu'),
+    headerMenu = document.querySelector('.header__menu'),
+    headerList = document.querySelector('.header__list');
+
+  burger.addEventListener('click', () => {
+    // burger.classList.add('active');
+    headerMenu.classList.add('active');
+    headerList.classList.add('active');
+  });
+
+  headerMenu.addEventListener('click', (e) => {
+    const event = e.target;
+    if (event.classList.contains('header__menu')) {
+      burger.classList.remove('active');
+      headerMenu.classList.remove('active');
+      headerList.classList.remove('active');
+    }
+
+  });
 
 
 });
