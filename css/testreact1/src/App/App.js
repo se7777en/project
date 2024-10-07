d20e51f32fd1fbaa6dc2765f82fa29b1a85feeea
Melik <melikpapikyan@gmail.com>
2024-10-07 12:45:03 +0400

    -areact

--- css/testreact1/src/App/App.js
+++ css/testreact1/src/App/App.js
@@ -1,20 +1,41 @@
 // import logo from './logo.svg';
 
-import {useState} from 'react'
+import { useCallback, useState, useEffect } from 'react'
 import './App.css';
 
 
 
-function App() {
+const Form = () => {
 
-  const [value, setValue] = useState(0);
+  const [val, setVal] = useState(0);
+  const [toggle, setToggle] = useState(false);
 
- function changeValue (i) {
-  setValue(value => value + i)
- }
+  function changeValue(i) {
+    setVal(value => value + i)
+  }
+
+  function changeToggle() {
+    setToggle(toggle => !toggle)
+  }
+
+
+
+
+
+  const printLog = useCallback(() => {
+    return (
+      ['1']
+    )
+  },[val])
+
 
   return (
     <>
+      {
+        //  printLog()
+         <PrintCallback printLog={printLog} />
+      }
+
       <div className="cotainer">
         <h1 className="header">form</h1>
         <form className="form" action="#!">
@@ -22,19 +43,40 @@ function App() {
           <input type="text" placeholder="enter your email" />
           <textarea className="textarea" placeholder='Text'></textarea>
 
-          <div className="form__num">{value}</div>
+          <div className="form__num">{val}</div>
           <div className="form__btns">
             <button className='submit' type='submit'>send form</button>
             <div className="form__btns-left">
               <button onClick={() => changeValue(1)} className='btn'>+1</button>
               <button onClick={() => changeValue(-1)} className='btn' >-1</button>
-              <button className='btn'>btn</button>
+              <button onClick={changeToggle} className='btn'>{toggle ? 'true' : 'false'}</button>
             </div>
           </div>
         </form>
       </div>
     </>
   );
+}
+
+const PrintCallback = ({ printLog }) => {
+  const [loging, setLoging] = useState([]);
+
+  useEffect(() => {
+    setLoging(printLog())
+  }, [printLog])
+
+  return (
+    <>
+       {console.log(loging)}
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <Form />
+  )
 }
 
 export default App;
