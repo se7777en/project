67039f1ffd56c22f84967fdd27c3b923bfd5dffc
Melik <melikpapikyan@gmail.com>
2024-10-16 13:44:47 +0400

    -ahooks

--- css/testreact1/src/index.js
+++ css/testreact1/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/App';
+// import App from './App/useCallBack';
+//  import App1 from './App/useMemo';
+ import App2 from './App/useRef';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <App1 /> */}
+    <App2 />
   </React.StrictMode>
 );
