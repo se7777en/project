82d264a48cc69f50cdd5617031595c23081a5f79
Melik <melikpapikyan@gmail.com>
2024-10-20 23:19:56 +0400

    -acurrency

--- css/testreact1/src/index.js
+++ css/testreact1/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App/useCallBack';
 //  import App1 from './App/useMemo';
- import App2 from './App/useRef';
+//  import App2 from './App/useRef';
+  import App3 from './App/currency/currency';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     {/* <App /> */}
     {/* <App1 /> */}
-    <App2 />
-  </React.StrictMode>
+    {/* <App2 /> */}
+    <App3 />
+  </>
 );
