1a84afacb0103c881337275d1dc857c3d2fd0bb3
Melik <melikpapikyan@gmail.com>
2024-10-27 23:12:36 +0400

    -amarvel__components_hook

--- JavaScript_in_work/REACT/Marvell_components_hooks/src/index.js
+++ JavaScript_in_work/REACT/Marvell_components_hooks/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 // import MarvelService from './services/MarvelService';
 
@@ -12,10 +12,18 @@ import './style/style.scss';
 // marvelService.getCharacter(1011052).then(res => console.log(res));
 //marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
