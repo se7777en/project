6b07e530a7d334cbfd2f73564fbbfc2fa19d7910
Melik <melikpapikyan@gmail.com>
2024-10-28 11:18:21 +0400

    -aMarvell_components_hook_router

--- JavaScript_in_work/REACT/Marvell_components_hooks/src/components/appHeader/AppHeader.js
+++ JavaScript_in_work/REACT/Marvell_components_hooks/src/components/appHeader/AppHeader.js
@@ -1,13 +1,18 @@
 import './appHeader.scss';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Link, NavLink } from 'react-router-dom';
 =======
 
 >>>>>>> 80c569597 (-ajavascript_in_work)
+=======
+import { Link, NavLink } from 'react-router-dom';
+>>>>>>> 5b640d350 (-aMarvell_components_hook_router)
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
+<<<<<<< HEAD
 <<<<<<< HEAD
                 <Link to="/">
                     <span>Marvel</span> information portal
@@ -23,15 +28,27 @@ const AppHeader = () => {
                         to="/comics">Comics</NavLink></li>
 =======
                 <a href="#!">
+=======
+                <Link to="/">
+>>>>>>> 5b640d350 (-aMarvell_components_hook_router)
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
+<<<<<<< HEAD
                     <li><a href="#!">Characters</a></li>
                     /
                     <li><a href="#!">Comics</a></li>
 >>>>>>> 80c569597 (-ajavascript_in_work)
+=======
+                    <li><NavLink end
+                     style={({isActive}) => ({color: isActive ? '#9f0013': 'inherit'})} 
+                     to="/">Characters</NavLink></li>
+                    <li><NavLink end 
+                    style={({isActive}) => ({color: isActive ? '#9f0013': 'inherit'})} 
+                    to="/comics">Comics</NavLink></li>
+>>>>>>> 5b640d350 (-aMarvell_components_hook_router)
                 </ul>
             </nav>
         </header>
--- JavaScript_in_work/REACT/Marvell_components_hooks/src/components/pages/index.js
+++ JavaScript_in_work/REACT/Marvell_components_hooks/src/components/pages/index.js
@@ -1,6 +1,11 @@
 import MainPage from "./MainPage";
 import ComicsPage from "./ComicsPage";
+<<<<<<< HEAD
 import Page404 from "./404";
 import SingleComicPage from "./SingleComicPage";
 
 export { MainPage, ComicsPage, Page404, SingleComicPage };
+=======
+
+export { MainPage, ComicsPage };
+>>>>>>> 5b640d350 (-aMarvell_components_hook_router)
