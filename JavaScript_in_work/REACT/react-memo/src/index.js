25f6e8f8d1d64360ce080bc1f8bea578e4547ee7
Melik <melikpapikyan@gmail.com>
2024-11-11 19:21:40 +0400

    -ajs-book

--- JavaScript_in_work/REACT/react-memo/src/index.js
+++ JavaScript_in_work/REACT/react-memo/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Memo from './memo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// import Memo from './memo';
 // import PureComponent from './PureComponent';
 // import ShouldComponentUpdate from './ShouldComponentUpdate';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import Context from './ReactContext_class';
+// import ReactContextFunc from './ReactContext_func';
+// import CombinedContext from './CombinedContext/Main';
+   import USEReducer from './useReducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Memo />
+    {/* <Memo /> */}
     {/* <PureComponent /> */}
     {/* <ShouldComponentUpdate /> */}
+    {/* <Context /> */}
+    {/* <ReactContextFunc /> */}
+    {/* <CombinedContext /> */}
+    <USEReducer />
   </React.StrictMode>
 );
 
