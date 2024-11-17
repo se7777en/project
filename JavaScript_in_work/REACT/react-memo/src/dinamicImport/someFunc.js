ca73cf2818aea6395f2fb9cc617e12b73b999f5b
Melik <melikpapikyan@gmail.com>
2024-11-17 23:32:33 +0400

    -areact

---
+++ JavaScript_in_work/REACT/react-memo/src/dinamicImport/someFunc.js
@@ -0,0 +1,10 @@
+export function logger() {
+    console.log('logger');
+}
+export function logger2() {
+    console.log('logger2');
+}
+
+export default function logger3() {
+    console.log('logger3');
+}
