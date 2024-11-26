18cfaa2125125cb22bc407d9bde3cbfcdb35fd69
Melik <melikpapikyan@gmail.com>
2024-11-26 22:59:40 +0400

    -apr

--- JavaScript_in_work/REACT/react-memo/src/index.js
+++ JavaScript_in_work/REACT/react-memo/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -38,5 +39,19 @@ root.render(
     <HOC />
   </>
   //React.StrictMode
+=======
+import Memo from './memo';
+// import PureComponent from './PureComponent';
+// import ShouldComponentUpdate from './ShouldComponentUpdate';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Memo />
+    {/* <PureComponent /> */}
+    {/* <ShouldComponentUpdate /> */}
+  </React.StrictMode>
+>>>>>>> e5bfd7728 (-amemo)
 );
 
